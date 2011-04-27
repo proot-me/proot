a5bf67b938e0937ba15db7db4ab1445aff94bf15
Cedric VINCENT <cedric.vincent@st.com>
2011-04-27 07:45:55 +0200

    Remove duplicate call to poke_ureg() in the syscall exit sequence.

--- src/sysnum_exit.c
+++ src/sysnum_exit.c
@@ -36,13 +36,6 @@ case __NR_getcwd:
 	}
 
 	status = detranslate_addr(child, child->output, result, GETCWD);
-	if (status < 0)
-		break;
-
-	status = poke_ureg(child, SYSARG_RESULT, (word_t) status);
-	if (status < 0)
-		goto end;
-
 	break;
 
 case __NR_readlink:
@@ -71,13 +64,6 @@ case __NR_readlinkat:
 	assert((int) result <= status);
 
 	status = detranslate_addr(child, child->output, result, READLINK);
-	if (status < 0)
-		break;
-
-	status = poke_ureg(child, SYSARG_RESULT, (word_t) status);
-	if (status < 0)
-		goto end;
-
 	break;
 
 default:
