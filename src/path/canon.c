proot-me/proot

commit d2175fc360c2f7d4f67ff9a15dc932e608982b23

Fix detranslation of asymmetric bindings (getcwd(2) & readlink*(2)).
getcwd(2)
---------

Use the size as requested by the program instead of the size reported
by the kernel, otherwise the sanity check "new_size <= size" will
never pass in such cases:

    proot -m /tmp:/longer-tmp -w /longer-tmp ${ROOTFS} /bin/pwd

where ${ROOTFS} != "/".

readlink*(2)
------------

The function detranslate_addr() was splitted in two parts because its
previous design was too error prone, mainly due to the missing NULL
terminating byte.  Prior this patch, the command:

    proot -m /proc -m /tmp:/asym -w /asym / /bin/readlink /proc/self/cwd

returned "/tmp" instead of "/asym".

src/path/path.c
@@ -328,10 +328,11 @@ end:
 }
 
 /**
- * Remove the leading "root" part of a previously translated @path. It
- * returns 0 if the leading part was not the "root", otherwise it
- * returns the size in bytes of the updated @path, including the
- * end-of-string terminator. On error it returns -errno.
+ * Remove/substitute the leading part of a "translated" @path.  It
+ * returns 0 if no transformation is required (ie. symmetric binding),
+ * otherwise it returns the size in bytes of the updated @path,
+ * including the end-of-string terminator.  On error it returns
+ * -errno.
  */
 int detranslate_path(char path[PATH_MAX], int sanity_check)
 {
@@ -348,16 +349,13 @@ int detranslate_path(char path[PATH_MAX], int sanity_check)
 	if (path[0] != '/')
 		return 0;
 
-	/* Check if it is a translatable path. */
-	if (sanity_check) {
-		switch (substitute_binding(BINDING_REAL, path)) {
-		case 0:
-			return 0;
-		case 1:
-			return strlen(path) + 1;
-		default:
-			break;
-		}
+	switch (substitute_binding(BINDING_REAL, path)) {
+	case 0:
+		return 0;
+	case 1:
+		return strlen(path) + 1;
+	default:
+		break;
 	}
 
 	/* Ensure the path is within the new root. */

src/syscall/exit.c
@@ -22,48 +22,142 @@
  * Author: Cedric VINCENT (cedric.vincent@st.com)
  */
 
-/* Keep in sync' with detranslate_addr(). */
 switch (tracee->sysnum) {
-case PR_getcwd:
+case PR_getcwd: {
+	char path[PATH_MAX];
+	size_t new_size;
+	size_t size;
+
 	result = peek_ureg(tracee, SYSARG_RESULT);
 	if (errno != 0) {
 		status = -errno;
 		goto end;
 	}
+
+	/* Error reported by the kernel.  */
 	if ((int) result < 0) {
 		status = 0;
 		goto end;
 	}
 
-	status = detranslate_addr(tracee, tracee->output, result, GETCWD);
+	size = (size_t) peek_ureg(tracee, SYSARG_2);
+	if (errno != 0) {
+		status = -errno;
+		goto end;
+	}
+
+	if (size > PATH_MAX) {
+		status = -ENAMETOOLONG;
+		break;
+	}
+
+	if (size == 0) {
+		status = -EINVAL;
+		break;
+	}
+
+	/* The kernel does put the terminating NULL byte for
+	 * getcwd(2).  */
+	status = get_tracee_string(tracee, path, tracee->output, size);
+	if (status < 0)
+		goto end;
+
+	/* No '\0' were found, that shouldn't happen...  */
+	if (status >= size) {
+		status = -ERANGE;
+		break;
+	}
+
+	status = detranslate_path(path, 1);
+	if (status < 0)
+		break;
+
+	/* The original path doesn't require any transformation, i.e
+	 * it is a symetric binding.  */
+	if (status == 0)
+		goto end;
+
+	new_size = status;
+	if (new_size > size) {
+		status = -ERANGE;
+		break;
+	}
+
+	/* Overwrite the path.  */
+	status = copy_to_tracee(tracee, tracee->output, path, new_size);
+	if (status < 0)
+		goto end;
+
+	/* The value of "status" is used to update the returned value
+	 * in translate_syscall_exit().  */
+	status = new_size;
+}
 	break;
 
 case PR_readlink:
-case PR_readlinkat:
+case PR_readlinkat: {
+	char path[PATH_MAX];
+	size_t old_size;
+	size_t new_size;
+	size_t max_size;
+
 	result = peek_ureg(tracee, SYSARG_RESULT);
 	if (errno != 0) {
 		status = -errno;
 		goto end;
 	}
+
+	/* Error reported by the kernel.  */
 	if ((int) result < 0) {
 		status = 0;
 		goto end;
 	}
 
-	/* Avoid the detranslation of partial result. */
-	status = (int) peek_ureg(tracee, tracee->sysnum == PR_readlink ? SYSARG_3 : SYSARG_4);
+	old_size = result;
+
+	max_size = (size_t) peek_ureg(tracee, tracee->sysnum == PR_readlink ? SYSARG_3 : SYSARG_4);
 	if (errno != 0) {
 		status = -errno;
 		goto end;
 	}
-	if ((int) result == status) {
-		status = 0;
-		goto end;
+
+	if (max_size > PATH_MAX) {
+		status = -ENAMETOOLONG;
+		break;
 	}
 
-	assert((int) result <= status);
+	if (max_size == 0) {
+		status = -EINVAL;
+		break;
+	}
 
-	status = detranslate_addr(tracee, tracee->output, result, READLINK);
+	/* The kernel does NOT put the terminating NULL byte for
+	 * getcwd(2).  */
+	status = copy_from_tracee(tracee, path, tracee->output, old_size);
+	if (status < 0)
+		goto end;
+	path[old_size] = '\0';
+
+	status = detranslate_path(path, 1);
+	if (status < 0)
+		break;
+
+	/* The original path doesn't require any transformation, i.e
+	 * it is a symetric binding.  */
+	if (status == 0)
+		goto end;
+
+	new_size = (status - 1 < max_size ? status - 1 : max_size);
+
+	/* Overwrite the path.  */
+	status = copy_to_tracee(tracee, tracee->output, path, new_size);
+	if (status < 0)
+		goto end;
+
+	/* The value of "status" is used to update the returned value
+	 * in translate_syscall_exit().  */
+	status = new_size;
+}
 	break;
 
 case PR_uname:

src/syscall/syscall.c
@@ -158,66 +158,6 @@ static int translate_sysarg(struct tracee_info *tracee, enum sysarg sysarg, int 
 }
 
 /**
- * Detranslate the path pointed to by @addr in the @tracee's memory
- * space (@size bytes max). It returns the number of bytes used by the
- * new path pointed to by @addr, including the string terminator.
- *
- * Keep in sync' with sysnum_exit.c
- */
-#define GETCWD   1
-#define READLINK 2
-static int detranslate_addr(struct tracee_info *tracee, word_t addr, int size, int mode)
-{
-	char path[PATH_MAX];
-	int new_size;
-	int status;
-
-	assert(size >= 0);
-
-	/* Extract the original path, be careful since some syscalls
-	 * like readlink*(2) do not put a C string terminator. */
-	if (size >= PATH_MAX)
-		return -ENAMETOOLONG;
-
-	status = copy_from_tracee(tracee, path, addr, size);
-	if (status < 0)
-		return status;
-	path[size] = '\0';
-
-	/* Removes the leading "root" part. */
-	status = detranslate_path(path, mode == GETCWD);
-	if (status < 0)
-		return status;
-	new_size = status;
-
-	/* The original path doesn't need detranslation, i.e it is a
-	 * symetric binding. */
-	if (new_size == 0) {
-		new_size = size;
-		goto skip_overwrite;
-	}
-
-	if (mode == GETCWD) {
-		if (new_size > size)
-			return -ERANGE;
-	}
-	else { /* READLINK */
-		/* Don't include the terminating '\0'. */
-		new_size--;
-		if (new_size > size)
-			new_size = size;
-	}
-
-	/* Overwrite the path. */
-	status = copy_to_tracee(tracee, addr, path, new_size);
-	if (status < 0)
-		return status;
-
-skip_overwrite:
-	return new_size;
-}
-
-/**
  * Translate the input arguments of the syscall @tracee->sysnum in the
  * @tracee->pid process area. This function optionnaly sets
  * @tracee->output to the address of the output argument in the tracee's
@@ -397,7 +337,7 @@ static int translate_syscall_exit(struct tracee_info *tracee)
 		status = -ENOSYS;
 	}
 
-	/* "status" was updated in sysnum_exit.c.  */
+	/* "status" was updated in syscall/exit.c.  */
 	status = poke_ureg(tracee, SYSARG_RESULT, (word_t) status);
 end:
 	if (status > 0)

tests/pwd.c
@@ -18,5 +18,5 @@ int main(void)
 
 	puts(path);
 
-	exit(EXIT_FAILURE);
+	exit(EXIT_SUCCESS);
 }

tests/test-3dec4597.sh
@@ -1,0 +1,1 @@
+${PROOT} -m /tmp:/longer-tmp -w /longer-tmp ${ROOTFS} /bin/pwd

tests/test-d1da0d8d.sh
@@ -1,0 +1,2 @@
+${PROOT} -m /proc -m /tmp:/asym -w /asym ${ROOTFS} /bin/readlink /proc/self/cwd | grep '^/asym$'
+${PROOT} -m /proc -m /tmp:/asym -w /tmp  ${ROOTFS} /bin/readlink /proc/self/cwd | grep '^/tmp$'

commit 531ea14d224413a9b7ffb6f598434eac1e4aa909

Fix a regression introduced by d2175fc3 regarding absolute symlinks.

src/path/canon.c
@@ -161,7 +161,7 @@ int canonicalize(pid_t pid, const char *fake_path, int deref_final,
 
 		/* Remove the leading "root" part if needed, it's
 		 * useful for "/proc/self/cwd/" for instance. */
-		status = detranslate_path(tmp, 0);
+		status = detranslate_path(tmp, false);
 		if (status < 0)
 			return status;
 

src/path/path.c
@@ -282,7 +282,7 @@ int translate_path(struct tracee_info *tracee, char result[PATH_MAX], int dir_fd
 
 		/* Remove the leading "root" part of the base
 		 * (required!). */
-		status = detranslate_path(result, 1);
+		status = detranslate_path(result, true);
 		if (status < 0)
 			return status;
 	}
@@ -334,7 +334,7 @@ end:
  * including the end-of-string terminator.  On error it returns
  * -errno.
  */
-int detranslate_path(char path[PATH_MAX], int sanity_check)
+int detranslate_path(char path[PATH_MAX], bool sanity_check)
 {
 	size_t new_length;
 
@@ -360,7 +360,7 @@ int detranslate_path(char path[PATH_MAX], int sanity_check)
 
 	/* Ensure the path is within the new root. */
 	if (strncmp(path, root, root_length) != 0) {
-		if (sanity_check != 0)
+		if (sanity_check)
 			return -EPERM;
 		else
 			return 0;

src/path/path.h
@@ -28,6 +28,7 @@
 #include <sys/types.h> /* pid_t, */
 #include <fcntl.h> /* AT_FDCWD, */
 #include <limits.h> /* PATH_MAX, */
+#include <stdbool.h>
 
 #include "tracee/info.h"
 
@@ -40,7 +41,7 @@
 
 extern void init_module_path();
 extern int translate_path(struct tracee_info *tracee, char result[PATH_MAX], int dir_fd, const char *fake_path, int deref_final);
-extern int detranslate_path(char path[PATH_MAX], int sanity_check);
+extern int detranslate_path(char path[PATH_MAX], bool sanity_check);
 
 extern int join_paths(int number_paths, char result[PATH_MAX], ...);
 extern int next_component(char component[NAME_MAX], const char **cursor);

src/syscall/exit.c
@@ -68,7 +68,7 @@ case PR_getcwd: {
 		break;
 	}
 
-	status = detranslate_path(path, 1);
+	status = detranslate_path(path, true);
 	if (status < 0)
 		break;
 
@@ -138,7 +138,7 @@ case PR_readlinkat: {
 		goto end;
 	path[old_size] = '\0';
 
-	status = detranslate_path(path, 1);
+	status = detranslate_path(path, false);
 	if (status < 0)
 		break;
 

tests/GNUmakefile
@@ -23,7 +23,14 @@ test-%.c: setup $(ROOTFS)/bin/test-%
 # Build a clean rootfs
 
 setup: $(ROOTFS)/bin/true $(ROOTFS)/bin/false    \
-       $(ROOTFS)/bin/pwd  $(ROOTFS)/bin/readlink
+       $(ROOTFS)/bin/pwd  $(ROOTFS)/bin/readlink \
+       $(ROOTFS)/bin/abs-true $(ROOTFS)/bin/rel-true
+
+$(ROOTFS)/bin/abs-true:
+	ln -s /bin/true $@
+
+$(ROOTFS)/bin/rel-true:
+	ln -s ./true $@
 
 $(ROOTFS)/bin/%: # %.c
 	@test -e $(dir $@) || mkdir -p $(dir $@)

tests/test-d2175fc3.sh
@@ -1,0 +1,2 @@
+${PROOT} ${ROOTFS} /bin/readlink /bin/abs-true | grep '^/bin/true$'
+${PROOT} ${ROOTFS} /bin/readlink /bin/rel-true | grep '^\./true$'