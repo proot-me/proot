proot-me/proot

commit 0238c7f1c4d70abaa211c7e01fffda75faaea8f0

Add a path comparison function, finally.
String comparison can't be always used to compare paths, for instance
the two following paths are equal: "/bin" and "/bin/".

src/path/binding.c
@@ -131,7 +131,9 @@ void print_bindings()
 	struct binding *binding;
 
 	for (binding = bindings; binding != NULL; binding = binding->next) {
-		if (strcmp(binding->real.path, binding->location.path) == 0)
+		if (compare_paths2(binding->real.path, binding->real.length,
+				   binding->location.path, binding->location.length)
+			== PATHS_ARE_EQUAL)
 			notice(INFO, USER, "binding = %s", binding->real.path);
 		else
 			notice(INFO, USER, "binding = %s:%s",
@@ -159,6 +161,7 @@ int substitute_binding(int which, char path[PATH_MAX])
 	for (binding = bindings; binding != NULL; binding = binding->next) {
 		struct binding_path *ref;
 		struct binding_path *anti_ref;
+		enum path_comparison comparison;
 
 		if (!binding->sanitized)
 			continue;
@@ -179,12 +182,9 @@ int substitute_binding(int which, char path[PATH_MAX])
 			return -1;
 		}
 
-		/* Skip comparisons that obviously fail. */
-		if (ref->length > path_length)
-			continue;
-
-		if (   path[ref->length] != '/'
-		    && path[ref->length] != '\0')
+		comparison = compare_paths2(ref->path, ref->length, path, path_length);
+		if (   comparison != PATHS_ARE_EQUAL
+		    && comparison != PATH1_IS_PREFIX)
 			continue;
 
 		if (which == BINDING_REAL) {
@@ -193,7 +193,7 @@ int substitute_binding(int which, char path[PATH_MAX])
 			 *
 			 *     proot -m /usr:/location /usr/local/slackware
 			 */
-			if (root_length != 0 /* rootfs != "/" */
+			if (root_length != 1 /* rootfs != "/" */
 			    && belongs_to_guestfs(path))
 				continue;
 
@@ -202,12 +202,6 @@ int substitute_binding(int which, char path[PATH_MAX])
 			if (ref->length == 0 &&	path_length == 1)
 				path_length = 0;
 		}
-
-		/* Using strncmp(3) to compare paths works fine here
-		 * because both pathes were sanitized, i.e. there is
-		 * no redundant ".", ".." or "/". */
-		if (strncmp(ref->path, path, ref->length) != 0)
-			continue;
 
 		/* Is it a "symetric" binding?  */
 		if (binding->need_substitution == 0)
@@ -362,10 +356,10 @@ void init_bindings()
 			goto error;
 		}
 
-		/* TODO: use a real path comparison function, for instance
-		 * strcmp(3) reports a wrong result with "/foo" and "/foo/."  */
-		binding->need_substitution = strcmp(binding->real.path, binding->location.path);
 		binding->location.length = strlen(binding->location.path);
+		binding->need_substitution =
+			compare_paths(binding->real.path,
+				binding->location.path) != PATHS_ARE_EQUAL;
 
 		/* Remove the trailing slash as expected by substitute_binding(). */
 		if (binding->location.path[binding->location.length - 1] == '/') {

src/path/path.c
@@ -195,10 +195,7 @@ void init_module_path()
 	assert(strlen(config.guest_rootfs) < PATH_MAX);
 	strcpy(root, config.guest_rootfs);
 
-	if (strcmp(root, "/") == 0)
-		root_length = 0;
-	else
-		root_length = strlen(root);
+	root_length = strlen(root);
 	initialized = 1;
 
 	init_bindings();
@@ -210,18 +207,18 @@ void init_module_path()
  * It is useful when using a runner that needs shared libraries or
  * reads some configuration files, for instance.
  */
-static int is_delayed(struct tracee_info *tracee, const char *path)
+static bool is_delayed(struct tracee_info *tracee, const char *path)
 {
 	if (tracee->trigger == NULL)
-		return 0;
+		return false;
 
-	if (strcmp(tracee->trigger, path) != 0)
-		return 1;
+	if (compare_paths(tracee->trigger, path) != PATHS_ARE_EQUAL)
+		return true;
 
 	free(tracee->trigger);
 	tracee->trigger = NULL;
 
-	return 0;
+	return false;
 }
 
 /**
@@ -333,6 +330,7 @@ end:
  */
 int detranslate_path(char path[PATH_MAX], bool sanity_check, bool follow_binding)
 {
+	size_t prefix_length;
 	size_t new_length;
 
 	assert(initialized != 0);
@@ -357,25 +355,31 @@ int detranslate_path(char path[PATH_MAX], bool sanity_check, bool follow_binding
 		}
 	}
 
-	/* Ensure the path is within the new root. */
-	if (!belongs_to_guestfs(path)) {
+	switch (compare_paths2(root, root_length, path, strlen(path))) {
+	case PATH1_IS_PREFIX:
+		/* Remove the leading part, that is, the "root".  */
+
+		/* Special case when path to the guest rootfs == "/". */
+		prefix_length = (root_length == 1 ? 0 : root_length);
+
+		new_length = strlen(path) - prefix_length;
+		memmove(path, path + prefix_length, new_length);
+
+		path[new_length] = '\0';
+		break;
+
+	case PATHS_ARE_EQUAL:
+		/* Special case when path == root. */
+		new_length = 1;
+		strcpy(path, "/");
+		break;
+
+	default:
+		/* Ensure the path is within the new root.  */
 		if (sanity_check)
 			return -EPERM;
 		else
 			return 0;
-	}
-
-	/* Remove the leading part, that is, the "root". */
-	assert(strlen(path) >= root_length);
-	new_length = strlen(path) - root_length;
-	if (new_length != 0) {
-		memmove(path, path + root_length, new_length);
-		path[new_length] = '\0';
-	}
-	else {
-		/* Special case when path == root. */
-		new_length = 1;
-		strcpy(path, "/");
 	}
 
 	return new_length + 1;
@@ -387,9 +391,74 @@ int detranslate_path(char path[PATH_MAX], bool sanity_check, bool follow_binding
  */
 bool belongs_to_guestfs(char *t_path)
 {
-	/* Works only with translated paths!  */
-	return (strncmp(root, t_path, root_length) == 0
-		&& (t_path[root_length] == '\0' || t_path[root_length] == '/'));
+	enum path_comparison comparison;
+
+	comparison = compare_paths2(root, root_length, t_path, strlen(t_path));
+	return (comparison == PATHS_ARE_EQUAL || comparison == PATH1_IS_PREFIX);
+}
+
+/**
+ * Compare @path1 with @path2, which are respectively @length1 and
+ * @length2 long.
+ *
+ * This function works only with paths canonicalized in the same
+ * namespace (host/guest)!
+ */
+enum path_comparison compare_paths2(const char *path1, size_t length1,
+				const char *path2, size_t length2)
+{
+	size_t length_min;
+	bool is_prefix;
+	char sentinel;
+
+#if defined DEBUG_OPATH
+	assert(length(path1) == length1);
+	assert(length(path2) == length2);
+#endif
+
+	if (!length1 || !length2) {
+		return PATHS_ARE_NOT_COMPARABLE;
+	}
+
+	/* Remove potential trailing '/' for the comparison.  */
+	if (path1[length1 - 1] == '/')
+		length1--;
+
+	if (path2[length2 - 1] == '/')
+		length2--;
+
+	if (length1 < length2) {
+		length_min = length1;
+		sentinel = path2[length_min];
+	}
+	else {
+		length_min = length2;
+		sentinel = path1[length_min];
+	}
+
+	/* Optimize obvious cases.  */
+	if (sentinel != '/' && sentinel != '\0')
+		return PATHS_ARE_NOT_COMPARABLE;
+
+	is_prefix = (strncmp(path1, path2, length_min) == 0);
+
+	if (!is_prefix)
+		return PATHS_ARE_NOT_COMPARABLE;
+
+	if (length1 == length2)
+		return PATHS_ARE_EQUAL;
+	else if (length1 < length2)
+		return PATH1_IS_PREFIX;
+	else if (length1 > length2)
+		return PATH2_IS_PREFIX;
+
+	assert(0);
+	return PATHS_ARE_NOT_COMPARABLE;
+}
+
+enum path_comparison compare_paths(const char *path1, const char *path2)
+{
+	return compare_paths2(path1, strlen(path1), path2, strlen(path2));
 }
 
 typedef int (*foreach_fd_t)(pid_t pid, int fd, char path[PATH_MAX]);

src/path/path.h
@@ -55,6 +55,17 @@ extern int list_open_fd(pid_t pid);
 extern char root[PATH_MAX];
 extern size_t root_length;
 
+enum path_comparison {
+	PATHS_ARE_EQUAL = 0,
+	PATH1_IS_PREFIX = -1,
+	PATH2_IS_PREFIX = 1,
+	PATHS_ARE_NOT_COMPARABLE = 125,
+};
+
+extern enum path_comparison compare_paths(const char *path1, const char *path2);
+extern enum path_comparison compare_paths2(const char *path1, size_t length1,
+					const char *path2, size_t length2);
+
 /* Check if path interpretable relatively to dirfd, see openat(2) for details. */
 #define AT_FD(dirfd, path) ((dirfd) != AT_FDCWD && ((path) != NULL && (path)[0] != '/'))
 

commit 80d038225c4e2c7c39fa915f61e78ca69004a278

Fix support for "/" in asymmetric bindings.
Commit 0238c7f1 introduced a regression, moreover binding.c wasn't
migrated to the new path length policy yet ("/".length == 1 not 0).

src/path/binding.c
@@ -96,10 +96,6 @@ void bind_path(const char *host_path, const char *guest_path, bool must_exist)
 
 	binding->host.length = strlen(binding->host.path);
 
-	/* Special case when the host rootfs is bound. */
-	if (binding->host.length == 1)
-		binding->host.length = 0;
-
 	tmp = guest_path ? guest_path : host_path;
 	if (strlen(tmp) >= PATH_MAX) {
 		notice(ERROR, INTERNAL, "binding location \"%s\" is too long", tmp);
@@ -162,6 +158,7 @@ int substitute_binding(int which, char path[PATH_MAX])
 		struct binding_path *ref;
 		struct binding_path *anti_ref;
 		enum path_comparison comparison;
+		size_t new_length;
 
 		if (!binding->sanitized)
 			continue;
@@ -187,48 +184,72 @@ int substitute_binding(int which, char path[PATH_MAX])
 		    && comparison != PATH1_IS_PREFIX)
 			continue;
 
-		if (which == BINDING_HOST_REF) {
-			/* Don't substitute systematically the prefix of the
-			 * rootfs when used as an asymmetric binding, as with:
-			 *
-			 *     proot -m /usr:/location /usr/local/slackware
-			 */
-			if (root_length != 1 /* rootfs != "/" */
-			    && belongs_to_guestfs(path))
+		/* Don't substitute systematically the prefix of the
+		 * rootfs when used as an asymmetric binding, as with:
+		 *
+		 *     proot -m /usr:/location /usr/local/slackware
+		 */
+		if (   which == BINDING_HOST_REF
+		    && root_length != 1
+		    && belongs_to_guestfs(path))
 				continue;
-
-			/* Avoid an extra trailing '/' when in the
-			 * asymmetric binding of the host rootfs. */
-			if (ref->length == 0 &&	path_length == 1)
-				path_length = 0;
-		}
 
 		/* Is it a "symetric" binding?  */
 		if (binding->need_substitution == 0)
 			return 0;
 
-		/* Ensure the substitution will not create a buffer
-		 * overflow. */
-		if (path_length - ref->length + anti_ref->length >= PATH_MAX)  {
-			notice(WARNING, INTERNAL, "Can't handle binding %s: pathname too long");
-			return -1;
-		}
+		/* Substitute the leading ref' with anti_ref'.  */
+		if (anti_ref->length == 1) {
+			/* Special case when "-b /:/foo".  Substitute
+			 * "/foo/bin" with "/bin" not "//bin".  */
+			assert(which == BINDING_GUEST_REF);
 
-		/* Replace the guest path with the host path. */
-		memmove(path + anti_ref->length, path + ref->length, path_length - ref->length);
-		memcpy(path, anti_ref->path, anti_ref->length);
-		path[path_length - ref->length + anti_ref->length] = '\0';
-
-		/* Special case when the host rootfs is bound at
-		 * the guest path pointed to by path[]. */
-		if (path[0] == '\0') {
-			path[0] = '/';
-			path[1] = '\0';
+			new_length = path_length - ref->length;
+			if (new_length != 0)
+				memmove(path, path + ref->length, new_length);
+			else {
+				/* Translating "/".  */
+				path[0] = '/';
+				new_length = 1;
+			}
 		}
+		else if (ref->length == 1) {
+			/* Special case when "-b /:/foo". Substitute
+			 * "/bin" with "/foo/bin" not "/foobin".  */
+			assert(which == BINDING_HOST_REF);
+
+			new_length = anti_ref->length + path_length;
+			if (new_length >= PATH_MAX)
+				goto too_long;
+
+			if (path_length > 1) {
+				memmove(path + anti_ref->length, path, path_length);
+				memcpy(path, anti_ref->path, anti_ref->length);
+			}
+			else {
+				/* Translating "/".  */
+				memcpy(path, anti_ref->path, anti_ref->length);
+				new_length = anti_ref->length;
+			}
+		}
+		else {
+			/* Generic substitution case.  */
+
+			new_length = path_length - ref->length + anti_ref->length;
+			if (new_length >= PATH_MAX)
+				goto too_long;
+
+			memmove(path + anti_ref->length,
+				path + ref->length,
+				path_length - ref->length);
+			memcpy(path, anti_ref->path, anti_ref->length);
+		}
+		path[new_length] = '\0';
 
 		return 1;
 	}
 
+too_long:
 	return -1;
 }
 

tests/test-0238c7f1.sh
@@ -1,0 +1,7 @@
+if [ ! -x  ${ROOTFS}/bin/pwd ] || [ -z `which grep` ]; then
+    exit 125;
+fi
+
+${PROOT} -m /:/hostfs -w /hostfs/bin ${ROOTFS} pwd | grep '^/hostfs/bin$'
+${PROOT} -m /:/hostfs -w /hostfs ${ROOTFS} pwd | grep '^/hostfs$'
+${PROOT} -m /:/hostfs -w / ${ROOTFS} pwd | grep '^/$'