proot-me/proot

commit 1e8806e7ea022b5013ed6f9e278f2471a2e95fcf

Replace the execve/args module with the tracee/array one.
This new module is more generic and allows a lot of performance
improvements: lazy item fetching (done), write-back only if needed
(done), prefetch memory reads (todo), verctorize writes (todo).

src/execve/execve.c
@@ -28,10 +28,10 @@
 #include <errno.h>    /* E*, */
 
 #include "execve/execve.h"
-#include "execve/args.h"
 #include "execve/interp.h"
 #include "execve/elf.h"
 #include "execve/ldso.h"
+#include "tracee/array.h"
 #include "syscall/syscall.h"
 #include "notice.h"
 #include "path/path.h"
@@ -83,7 +83,7 @@ static int expand_interp(struct tracee *tracee,
 			 const char *u_path,
 			 char t_interp[PATH_MAX],
 			 char u_interp[PATH_MAX],
-			 char **argv[],
+			 struct array *argv,
 			 extract_interp_t callback,
 			 bool ignore_interpreter)
 {
@@ -124,8 +124,7 @@ static int expand_interp(struct tracee *tracee,
 
 	/* Sanity check: an interpreter doesn't request an[other]
 	 * interpreter on Linux.  In the case of ELF this check
-	 * ensures the interpreter is in the ELF format.  Note
-	 * 'u_interp' and 'argument' are actually not used. */
+	 * ensures the interpreter is in the ELF format.  */
 	status = callback(tracee, t_interp, dummy_path, dummy_arg);
 	if (status < 0)
 		return status;
@@ -145,15 +144,26 @@ static int expand_interp(struct tracee *tracee,
 	 * in the caller because it depends on the use of a runner.
 	 */
 
-	VERBOSE(3, "expand shebang: %s -> %s %s %s",
-		(*argv)[0], u_interp, argument, u_path);
+	VERBOSE(3, "expand shebang: -> %s %s %s", u_interp, argument, u_path);
 
-	if (argument[0] != '\0')
-		status = push_args(true, argv, 3, u_interp, argument, u_path);
-	else
-		status = push_args(true, argv, 2, u_interp, u_path);
-	if (status < 0)
-		return status;
+	if (argument[0] != '\0') {
+		status = resize_array(argv, 0, 2);
+		if (status < 0)
+			return status;
+
+		status = write_items(argv, 0, 3, u_interp, argument, u_path);
+		if (status < 0)
+			return status;
+	}
+	else {
+		status = resize_array(argv, 0, 1);
+		if (status < 0)
+			return status;
+
+		status = write_items(argv, 0, 2, u_interp, u_path);
+		if (status < 0)
+			return status;
+	}
 
 	/* Remember at this point t_interp is the translation of
 	 * u_interp. */
@@ -162,7 +172,11 @@ static int expand_interp(struct tracee *tracee,
 
 /**
  * Translate the arguments of the execve() syscall made by the @tracee
- * process. This syscall needs a very special treatment for script
+ * process.  This function return -errno if an error occured,
+ * otherwise the number of bytes allocated in the stack to store the
+ * new argv[] and envp[].
+ *
+ * The execve() syscall needs a very special treatment for script
  * files because according to "man 2 execve":
  *
  *     An interpreter script is a text file [...] whose first line is
@@ -200,14 +214,13 @@ int translate_execve(struct tracee *tracee)
 	char t_interp[PATH_MAX];
 	char u_interp[PATH_MAX];
 
-	char **envp = NULL;
-	char **argv = NULL;
+	struct array envp = { 0 };
+	struct array argv = { 0 };
 	char *argv0 = NULL;
 
 	bool ignore_elf_interpreter;
-	bool envp_has_changed = false;
-	bool argv_has_changed = false;
 	bool inhibit_rpath = false;
+	bool is_script;
 	int size = 0;
 	int status;
 
@@ -217,23 +230,34 @@ int translate_execve(struct tracee *tracee)
 	if (status < 0)
 		return status;
 
-	status = get_args(tracee, &argv, SYSARG_2);
+	status = fetch_array(tracee, &argv, peek_reg(tracee, SYSARG_2), 0);
 	if (status < 0)
 		goto end;
-	assert(argv != NULL);
 
-	if(config.qemu)
-		argv0 = strdup(argv[0]);
+	if (config.qemu) {
+		status = read_item_string(&argv, 0, &argv0);
+		if (status < 0)
+			goto end;
 
-	status = get_args(tracee, &envp, SYSARG_3);
+		/* Save the initial argv[0] since it will be replaced
+		 * by config.qemu[0].  */
+		if (argv0 != NULL)
+			argv0 = strdup(argv0);
+
+		status = fetch_array(tracee, &envp, peek_reg(tracee, SYSARG_3), 0);
+		if (status < 0)
+			goto end;
+
+		/* Environment variables should be compared with the
+		 * "name" part in the "name=value" string format.  */
+		envp.compare_item = (compare_item_t)compare_item_env;
+	}
+
+	status = expand_interp(tracee, u_path, t_interp, u_interp, &argv,
+			       extract_script_interp, false);
 	if (status < 0)
 		goto end;
-	assert(envp != NULL);
-
-	status = expand_interp(tracee, u_path, t_interp, u_interp, &argv, extract_script_interp, false);
-	if (status < 0)
-		goto end;
-	argv_has_changed = (status > 0);
+	is_script = (status > 0);
 
 	/* "/proc/self/exe" points to a canonicalized path -- from the
 	 * guest point-of-view --, hence detranslate_path(t_interp).
@@ -248,23 +272,34 @@ int translate_execve(struct tracee *tracee)
 		/* Prepend the QEMU command to the initial argv[] if
 		 * it's a "foreign" binary.  */
 		if (!is_host_elf(t_interp)) {
-			status = push_args(false, &argv, 1, u_interp);
+			int i;
+
+			status = resize_array(&argv, 0, 3);
 			if (status < 0)
 				goto end;
 
-			status = push_args(true, &argv, -1, config.qemu);
+			status = write_items(&argv, 0, 3, config.qemu[0], "-0",
+					!is_script && argv0 != NULL ? argv0 : u_interp);
 			if (status < 0)
 				goto end;
 
-			envp_has_changed = ldso_env_passthru(&envp, &argv, "-E", "-U");
+			status = ldso_env_passthru(&envp, &argv, "-E", "-U");
+			if (status < 0)
+				goto end;
 
-			/* Errors are not fatal here.  */
-			if (!argv_has_changed)
-				push_args(false, &argv, 2, "-0", argv0);
-			else
-				push_args(false, &argv, 2, "-0", u_interp);
+			/* Compute the number of QEMU's arguments and
+			 * add them to the modified argv[].  */
+			for (i = 1; config.qemu[i] != NULL; i++)
+				;
+			status = resize_array(&argv, 1, i - 1);
+			if (status < 0)
+				goto end;
 
-			argv_has_changed = true;
+			for (i--; i > 0; i--) {
+				status = write_item(&argv, i, config.qemu[i]);
+				if (status < 0)
+					goto end;
+			}
 
 			/* Launch the runner actually. */
 			strcpy(t_interp, config.qemu[0]);
@@ -281,7 +316,6 @@ int translate_execve(struct tracee *tracee)
 		if (status < 0)
 			goto end;
 		inhibit_rpath = (status > 0);
-		envp_has_changed = true;
 	}
 
 	/* Dont't use the ELF interpreter as a loader if the host one
@@ -291,18 +325,22 @@ int translate_execve(struct tracee *tracee)
 	ignore_elf_interpreter = strcmp(config.guest_rootfs, "/") == 0
 				 || (config.qemu != NULL && !inhibit_rpath);
 
-	status = expand_interp(tracee, u_interp, t_interp, u_path /* dummy */, &argv, extract_elf_interp, ignore_elf_interpreter);
+	status = expand_interp(tracee, u_interp, t_interp, u_path /* dummy */,
+			       &argv, extract_elf_interp, ignore_elf_interpreter);
 	if (status < 0)
 		goto end;
-	argv_has_changed = argv_has_changed || (status > 0);
 
 	if (status > 0 && inhibit_rpath) {
 		/* Tell the dynamic linker to ignore RPATHs specified
 		 * in the *main* program.  To disable the RPATH
 		 * mechanism globally, we have to list all objects
 		 * here (NYI).  Errors are not fatal here.  */
-		status = push_args(false, &argv, 2, "--inhibit-rpath", "''");
-		argv_has_changed = argv_has_changed || (status > 0);
+		status = resize_array(&argv, 1, 2);
+		if (status >= 0) {
+			status = write_items(&argv, 1, 2, "--inhibit-rpath", "''");
+			if (status < 0)
+				goto end;
+		}
 	}
 
 	VERBOSE(4, "execve: %s", t_interp);
@@ -311,27 +349,27 @@ int translate_execve(struct tracee *tracee)
 	if (status < 0)
 		goto end;
 
-	/* Rebuild argv[] only if something has changed. */
-	if (argv_has_changed) {
-		size = set_args(tracee, argv, SYSARG_2);
-		if (size < 0) {
-			status = size;
-			goto end;
-		}
+	status = push_array(&argv);
+	if (status < 0)
+		goto end;
+	if (status > 0) {
+		size += status;
+		poke_reg(tracee, SYSARG_2, peek_reg(tracee, STACK_POINTER));
 	}
 
-	/* Rebuild envp[] only if something has changed. */
-	if (envp_has_changed) {
-		size = set_args(tracee, envp, SYSARG_3);
-		if (size < 0) {
-			status = size;
-			goto end;
-		}
+	status = push_array(&envp);
+	if (status < 0)
+		goto end;
+	if (status > 0) {
+		size += status;
+		poke_reg(tracee, SYSARG_3, peek_reg(tracee, STACK_POINTER));
 	}
+
+	status = 0;
 
 end:
-	free_args(envp);
-	free_args(argv);
+	free_array(&argv);
+	free_array(&envp);
 
 	if (argv0 != NULL) {
 		free(argv0);
@@ -341,5 +379,5 @@ end:
 	if (status < 0)
 		return status;
 
-	return size + status;
+	return size;
 }

src/GNUmakefile
@@ -15,7 +15,6 @@ LDFLAGS  =
 OBJECTS = cli.o			\
 	config.o		\
 	notice.o		\
-	execve/args.o		\
 	execve/execve.o		\
 	execve/interp.o		\
 	execve/elf.o		\
@@ -26,6 +25,7 @@ OBJECTS = cli.o			\
 	path/proc.o		\
 	syscall/syscall.o	\
 	tracee/tracee.o		\
+	tracee/array.o		\
 	tracee/mem.o		\
 	tracee/reg.o		\
 	tracee/event.o

src/config.c
@@ -22,11 +22,12 @@
 
 #include <stdbool.h> /* bool, true, false, */
 #include <string.h>  /* string(3), */
+#include <stdlib.h>  /* ssize_t, */
+#include <linux/limits.h> /* ARG_MAX, */
 
 #include "config.h"
 #include "notice.h"
 #include "path/binding.h"
-#include "execve/args.h"
 
 static void print_argv(const char *prompt, char **argv)
 {

src/execve/args.c
@@ -1,406 +1,0 @@
-/* -*- c-set-style: "K&R"; c-basic-offset: 8 -*-
- *
- * This file is part of PRoot.
- *
- * Copyright (C) 2010, 2011, 2012 STMicroelectronics
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301 USA.
- */
-
-#include <stdlib.h>     /* *alloc(3), free(3), */
-#include <string.h>     /* string(3), */
-#include <errno.h>      /* E*, */
-#include <stdarg.h>     /* va_*(3), */
-#include <assert.h>     /* assert(3), */
-
-#include "execve/args.h"
-#include "tracee/tracee.h"
-#include "tracee/reg.h"
-#include "tracee/mem.h"
-#include "tracee/abi.h"
-#include "notice.h"
-#include "syscall/syscall.h"
-#include "config.h"
-
-/**
- * Replace the value of the environment @entry (format is
- * "variable=value") with @value if this latter is not NULL,
- * otherwise with "".
- */
-int replace_env_entry(char **entry, const char *new_value)
-{
-	size_t name_length;
-	size_t new_value_length;
-	char *tmp;
-
-	assert(entry);
-
-	if (!new_value) {
-		(*entry)[0] = '\0';
-		return 0;
-	}
-
-	name_length = strcspn(*entry, "=");
-	new_value_length = strlen(new_value);
-
-	tmp = realloc(*entry, (name_length + 1 + new_value_length + 1) * sizeof(char));
-	if (tmp == NULL)
-		return  -ENOMEM;
-	*entry = tmp;
-
-	strcpy(*entry + name_length + 1, new_value);
-	return 0;
-}
-
-/**
- * Check if the environment @variable has the given @name.
- */
-inline bool check_env_entry_name(const char *variable, const char *name)
-{
-	size_t length = strlen(name);
-
-	return (variable[0] == name[0]
-		&& length < strlen(variable)
-		&& variable[length] == '='
-		&& strncmp(variable, name, length) == 0);
-}
-
-/**
- * Get the environment entry for the given variable @name.
- * The returned value is a pointer to the variable definition
- * string inside the given @envp array or NULL if undefined.
- */
-char **get_env_entry(char *envp[], const char *name)
-{
-	int i;
-	assert(envp);
-	assert(name);
-
-	for (i = 0; envp[i] != NULL; i++) {
-		if (check_env_entry_name(envp[i], name))
-			break;
-	}
-	if (envp[i] == NULL)
-		return NULL;
-	return &envp[i];
-}
-
-/**
- * Add the new environment variable @name into @envp[] with the given
- * @value (do nothing if this latter is NULL).
- */
-int new_env_entry(char **envp[], const char *name, const char *value)
-{
-	size_t new_size;
-	size_t length;
-	char **tmp;
-	int i;
-
-	assert(envp);
-	assert(name);
-
-	if (!value)
-		return 0;
-
-	/* Compute the number of entries in envp[].  */
-	for (i = 0; (*envp)[i] != NULL; i++)
-		;
-
-	/* Allocate a new entry + the terminating NULL pointer.  */
-	new_size = i + 2;
-
-	tmp = realloc(*envp, new_size * sizeof(char *));
-	if (tmp == NULL)
-		return -ENOMEM;
-	*envp = tmp;
-
-	length = strlen(name) + 1 /* = */ + strlen(value);
-	(*envp)[new_size - 2] = calloc(length + 1, sizeof(char));
-	if ((*envp)[new_size - 2] == NULL)
-		return -ENOMEM;
-
-	strcpy((*envp)[new_size - 2], name);
-	strcat((*envp)[new_size - 2], "=");
-	strcat((*envp)[new_size - 2], value);
-
-	(*envp)[new_size - 1] = NULL;
-
-	return 0;
-}
-
-/**
- * Add @nb_new_args new C strings to the argument list *@args.  If
- * @replace_argv0 is true then the previous args[0] is freed and
- * replaced with the first new entry.  If @nb_new_args is less than
- * zero, then the first variadic parameter is used as a table of
- * pointers to the new arguments.  This function returns -errno if an
- * error occured, otherwise 0.
- *
- *  Technically:
- *
- *    | old[0] | old[1] | ... | old[n] |
- *
- *  becomes as follow when !replace_argv0:
- *
- *    | old[0] | new[0] | ... | new[nb - 1] | old[1] | ... | old[n] |
- *
- *  otherwise as follow:
- *
- *    | new[0] | ... | new[nb - 1] | old[1] | ... | old[n] |
- */
-int push_args(bool replace_argv0, char **args[], int nb_new_args, ...)
-{
-	char **dest_old_args;
-	char **new_args;
-	va_list va_args;
-	void *tmp;
-
-	int nb_moved_old_args;
-	int nb_old_args;
-	int i;
-
-	if (nb_new_args < 0) {
-		/* Use the first variadic parameter as a table of
-		 * pointers to the new arguments. */
-		va_start(va_args, nb_new_args);
-		new_args = va_arg(va_args, char **);
-		va_end(va_args);
-
-		/* Compute nb_new_args.  */
-		for (i = 0; new_args[i] != NULL; i++)
-			;
-		nb_new_args = i;
-	}
-	else
-		new_args = NULL;
-
-	assert(nb_new_args > 0);
-
-	/* Count the number of old entries... */
-	for (i = 0; (*args)[i] != NULL; i++)
-		;
-	nb_old_args = i + 1; /* ... including NULL terminator slot... */
-
-	/* ... minus one if the args[0] slot is re-used. */
-	if (replace_argv0)
-		nb_old_args--;
-	assert(nb_old_args > 0);
-
-	/* Don't kill *args if the reallocation has failed, all
-	 * entries will be freed in translate_execve(). */
-	tmp = realloc(*args, (nb_new_args + nb_old_args) * sizeof(char *));
-	if (tmp == NULL)
-		return -ENOMEM;
-	*args = tmp;
-
-	/* Compute the new position and the number of moved old
-	 * entries: they are shifted to the right.  */
-	nb_moved_old_args = nb_old_args;
-	dest_old_args = *args + nb_new_args;
-	if (!replace_argv0) {
-		dest_old_args++;
-		nb_moved_old_args--;
-	}
-	else {
-		free(*args[0]);
-		*args[0] = NULL;
-	}
-
-	/* Move the old entries to let space at the beginning for the
-	 * new ones, note that args[0] is not moved whether it is
-	 * overwritten or not. */
-	memmove(dest_old_args, *args + 1, nb_moved_old_args * sizeof(char *));
-
-	/* Each new entry will be allocated into the heap since we
-	 * don't rely on the liveness of the input parameters. */
-	va_start(va_args, nb_new_args);
-	for (i = 0; i < nb_new_args; i++) {
-		int dest_index = i;
-		char *new_arg;
-
-		if (new_args)
-			new_arg = new_args[i];
-		else
-			new_arg = va_arg(va_args, char *);
-
-		if (!replace_argv0)
-			dest_index++;
-
-		(*args)[dest_index] = strdup(new_arg);
-		if ((*args)[dest_index] == NULL) {
-			va_end(va_args);
-			return -ENOMEM;
-		}
-	}
-	va_end(va_args);
-
-	return 0;
-}
-
-/**
- * Copy the *@args[] of the current execve(2) from the memory space of
- * the @tracee process.  This function returns -errno if an error
- * occured, otherwise 0.
- */
-int get_args(const struct tracee *tracee, char **args[], enum reg reg)
-{
-	word_t tracee_args;
-	word_t argp;
-	int nb_args;
-	int status;
-	int i;
-
-	tracee_args = peek_reg(tracee, reg);
-
-	/* Compute the number of entries in args[]. */
-	for (i = 0; ; i++) {
-		argp = peek_mem(tracee, tracee_args + i * sizeof_word(tracee));
-		if (errno != 0)
-			return -EFAULT;
-
-		/* End of args[]. */
-		if (argp == 0)
-			break;
-	}
-	nb_args = i;
-
-	*args = calloc(nb_args + 1, sizeof(char *));
-	if (*args == NULL)
-		return -ENOMEM;
-
-	(*args)[nb_args] = NULL;
-
-	/* Slurp arguments until the end of args[]. */
-	for (i = 0; i < nb_args; i++) {
-		char arg[ARG_MAX];
-
-		argp = peek_mem(tracee, tracee_args + i * sizeof_word(tracee));
-		if (errno != 0)
-			return -EFAULT;
-
-		assert(argp != 0);
-
-		status = read_string(tracee, arg, argp, ARG_MAX);
-		if (status < 0)
-			return status;
-		if (status >= ARG_MAX)
-			return -ENAMETOOLONG;
-
-		(*args)[i] = strdup(arg);
-		if ((*args)[i] == NULL)
-			return -ENOMEM;
-	}
-
-	return 0;
-}
-
-/**
- * Copy the @args[] to the memory space of the @tracee process.
- * This function returns -errno if an error occured, otherwise 0.
- *
- * Technically, we use the memory below the stack pointer to store the
- * new arguments and the new array of pointers to these arguments:
- *
- *                                          <- stack pointer
- *                                                          \
- *       args[]           args1              args0           \
- *     /                       \                  \           \
- *    | args[0] | args[1] | ... | "/bin/script.sh" | "/bin/sh" |
- */
-int set_args(struct tracee *tracee, char *args[], enum reg reg)
-{
-	word_t *tracee_args;
-
-	word_t previous_sp = 0;
-	word_t tracee_argp = 0;
-	word_t argp;
-
-	int nb_args;
-	size_t size;
-	long status;
-	int i;
-
-	/* Compute the number of entries. */
-	for (i = 0; args[i] != NULL; i++)
-		VERBOSE(4, "set args[%d] = %s", i, args[i]);
-
-	nb_args = i + 1;
-	tracee_args = calloc(nb_args, sizeof(word_t));
-	if (tracee_args == NULL)
-		return -ENOMEM;
-
-	/* Copy the new arguments in the tracee's stack. */
-	previous_sp = peek_reg(tracee, STACK_POINTER);
-
-	argp = previous_sp;
-	for (i = 0; args[i] != NULL; i++) {
-		size = strlen(args[i]) + 1;
-		argp -= size;
-
-		status = write_data(tracee, argp, args[i], size);
-		if (status < 0)
-			goto end;
-
-		tracee_args[i] = argp;
-	}
-
-	tracee_args[i] = 0;
-	tracee_argp = argp;
-
-	/* Copy the pointers to the new arguments backward in the stack. */
-	for (i = nb_args - 1; i >= 0; i--) {
-		tracee_argp -= sizeof_word(tracee);
-
-		poke_mem(tracee, tracee_argp, tracee_args[i]);
-		if (errno != 0) {
-			status = -EFAULT;
-			goto end;
-		}
-	}
-
-	/* Update the pointer to the new args[]. */
-	poke_reg(tracee, reg, tracee_argp);
-
-	/* Update the stack pointer to ensure [internal] coherency. It prevents
-	 * memory corruption if functions like set_sysarg_path() are called later. */
-	poke_reg(tracee, STACK_POINTER, tracee_argp);
-
-end:
-	free(tracee_args);
-	tracee_args = NULL;
-
-	if (status < 0)
-		return status;
-
-	return previous_sp - tracee_argp;
-}
-
-/** Free each element in @args[] */
-void free_args(char *args[])
-{
-	int i;
-
-	if (args == NULL)
-		return;
-
-	for (i = 0; args[i] != NULL; i++) {
-		free(args[i]);
-		args[i] = NULL;
-	}
-
-	free(args);
-	args = NULL;
-}

src/execve/args.h
@@ -1,42 +1,0 @@
-/* -*- c-set-style: "K&R"; c-basic-offset: 8 -*-
- *
- * This file is part of PRoot.
- *
- * Copyright (C) 2010, 2011, 2012 STMicroelectronics
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301 USA.
- */
-
-#ifndef ARGS_H
-#define ARGS_H
-
-#include <stdbool.h>
-#include "tracee/reg.h" /* enum reg */
-
-#ifndef ARG_MAX
-#define ARG_MAX 131072
-#endif
-
-extern bool check_env_entry_name(const char *variable, const char *name);
-extern char **get_env_entry(char *envp[], const char *name);
-extern int replace_env_entry(char **entry, const char *new_value);
-extern int new_env_entry(char **envp[], const char *name, const char *value);
-extern int push_args(bool replace_argv0, char **argv[], int nb_new_args, ...);
-extern int get_args(const struct tracee *tracee, char **argv[], enum reg reg);
-extern int set_args(struct tracee *tracee, char *argv[], enum reg reg);
-extern void free_args(char *argv[]);
-
-#endif /* ARGS_H */

src/execve/execve.h
@@ -24,6 +24,7 @@
 #define EXECVE_H
 
 #include "tracee/tracee.h"
+#include "tracee/array.h"
 
 extern int translate_execve(struct tracee *tracee);
 

src/execve/interp.c
@@ -22,7 +22,7 @@
 
 #include <fcntl.h>  /* open(2), */
 #include <unistd.h> /* read(2), close(2), */
-#include <limits.h> /* PATH_MAX, ARG_MAX, */
+#include <linux/limits.h> /* PATH_MAX, ARG_MAX, */
 #include <errno.h>  /* ENAMETOOLONG, */
 #include <string.h> /* strcpy(3), */
 

src/execve/interp.h
@@ -23,10 +23,9 @@
 #ifndef INTERP_H
 #define INTERP_H
 
-#include <limits.h>
+#include <linux/limits.h> /* PATH_MAX, ARG_MAX, */
 
 #include "tracee/tracee.h"
-#include "execve/args.h" /* ARG_MAX, */
 
 typedef int (* extract_interp_t)(const struct tracee *tracee,
 				 const char *t_path,

commit e8b27a5d08f83bb5d173c8f0df5f8b0a228065da

Fix execve() support for foreign binaries + add some execve() regression tests.
The program passed to QEMU was argv[0] ("ls" for example) instead of
the path to the foreign binary ("/bin/ls").  This regression was
introduced in commit 1e8806e7.

Change-Id: Ic7a9faf3382f796436a755cab7ca24383b28b679
Reported-By: Remi DURAFFORT <remi.duraffort@st.com>

src/execve/execve.c
@@ -271,8 +271,12 @@ int translate_execve(struct tracee *tracee)
 			if (status < 0)
 				goto end;
 
-			status = write_items(&argv, 0, 3, config.qemu[0], "-0",
-					!is_script && argv0 != NULL ? argv0 : u_interp);
+			/* For example, the second argument of:
+			 *     execve("/bin/true", { "true", NULL }, ...)
+			 * becomes:
+			 *     { "/usr/bin/qemu", "-0", "true", "/bin/true"}  */
+			status = write_items(&argv, 0, 4, config.qemu[0], "-0",
+					!is_script && argv0 != NULL ? argv0 : u_interp, u_interp);
 			if (status < 0)
 				goto end;
 

tests/GNUmakefile
@@ -49,7 +49,8 @@ check = case "$$?" in					\
 
 setup: $(ROOTFS)/bin/true $(ROOTFS)/bin/false    \
        $(ROOTFS)/bin/pwd  $(ROOTFS)/bin/readlink $(ROOTFS)/bin/symlink \
-       $(ROOTFS)/bin/abs-true $(ROOTFS)/bin/rel-true
+       $(ROOTFS)/bin/abs-true $(ROOTFS)/bin/rel-true $(ROOTFS)/bin/echo \
+       $(ROOTFS)/bin/argv0
 
 $(ROOTFS)/bin/abs-true:
 	@ln -fs /bin/true $@

tests/argv0.c
@@ -1,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	puts(argv[0]);
+	return 0;
+}

tests/echo.c
@@ -1,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+		puts(argv[i]);
+
+	return 0;
+}

tests/test-092c5e26.sh
@@ -1,0 +1,31 @@
+if [ ! -x  ${ROOTFS}/bin/echo ] ||  [ ! -x  ${ROOTFS}/bin/argv0 ] || [ -z `which mcookie` ] || [ -z `which grep` ] || [ -z `which cat` ] || [ -z `which rm` ]; then
+    exit 125;
+fi
+
+TMP=$(mcookie)
+TMP_ABS=/tmp/${TMP}
+
+${PROOT} -r ${ROOTFS} argv0      | grep '^argv0$'
+${PROOT} -r ${ROOTFS} /bin/argv0 | grep '^/bin/argv0$'
+
+cat > ${ROOTFS}/${TMP_ABS} <<EOF
+#!/bin/argv0
+test
+EOF
+chmod +x ${ROOTFS}/${TMP_ABS}
+
+env PATH=/tmp ${PROOT} -r ${ROOTFS} ${TMP} | grep '^/bin/argv0$'
+${PROOT} -r ${ROOTFS} ${TMP_ABS}           | grep '^/bin/argv0$'
+
+env PROOT_FORCE_FOREIGN_BINARY=1 PATH=/tmp:/bin ${PROOT} -r ${ROOTFS} -q echo ${TMP} | grep "^-U LD_LIBRARY_PATH -0 /bin/argv0 /bin/argv0 ${TMP_ABS}$"
+env PROOT_FORCE_FOREIGN_BINARY=1 ${PROOT} -r ${ROOTFS} -q echo ${TMP_ABS} | grep "^-U LD_LIBRARY_PATH -0 /bin/argv0 /bin/argv0 ${TMP_ABS}$"
+
+cat > ${ROOTFS}/${TMP_ABS} <<EOF
+FOREIGN BINARY FORMAT
+EOF
+chmod +x ${ROOTFS}/${TMP_ABS}
+
+env PATH=/tmp:/bin ${PROOT} -r ${ROOTFS} -q echo ${TMP} | grep "^-U LD_LIBRARY_PATH -0 ${TMP} ${TMP_ABS}$"
+${PROOT} -r ${ROOTFS} -q echo ${TMP_ABS}                | grep "^-U LD_LIBRARY_PATH -0 ${TMP_ABS} ${TMP_ABS}$"
+
+rm -f ${TMP_ABS}