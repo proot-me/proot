proot-me/proot

commit 294e958c2f9336aa640c5254788be7e0e66e7473

New coding-style: struct, enum, and union are now upper-case typedef.
This new coding-style is more compact: it saves a couple of wrapping lines
and about 2.7k characters ;)

Change-Id: If60bf0c5bf8d1b14b9c7d65d53f21211b2b90bf9

src/execve/elf.h
@@ -28,7 +28,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-struct elf_header32 {
+typedef struct {
 	unsigned char e_ident[EI_NIDENT];
 	uint16_t      e_type;
 	uint16_t      e_machine;
@@ -43,9 +43,9 @@ struct elf_header32 {
 	uint16_t      e_shentsize;
 	uint16_t      e_shnum;
 	uint16_t      e_shstrndx;
-};
+} ElfHeader32;
 
-struct elf_header64 {
+typedef struct {
 	unsigned char e_ident[EI_NIDENT];
 	uint16_t      e_type;
 	uint16_t      e_machine;
@@ -60,14 +60,14 @@ struct elf_header64 {
 	uint16_t      e_shentsize;
 	uint16_t      e_shnum;
 	uint16_t      e_shstrndx;
-};
+} ElfHeader64;
 
-union elf_header {
-	struct elf_header32 class32;
-	struct elf_header64 class64;
-};
+typedef union {
+	ElfHeader32 class32;
+	ElfHeader64 class64;
+} ElfHeader;
 
-struct program_header32 {
+typedef struct {
 	uint32_t   p_type;
 	uint32_t   p_offset;
 	uint32_t   p_vaddr;
@@ -76,9 +76,9 @@ struct program_header32 {
 	uint32_t   p_memsz;
 	uint32_t   p_flags;
 	uint32_t   p_align;
-};
+} ProgramHeader32;
 
-struct program_header64 {
+typedef struct {
 	uint32_t   p_type;
 	uint32_t   p_flags;
 	uint64_t   p_offset;
@@ -87,39 +87,39 @@ struct program_header64 {
 	uint64_t   p_filesz;
 	uint64_t   p_memsz;
 	uint64_t   p_align;
-};
+} ProgramHeader64;
 
-union program_header {
-	struct program_header32 class32;
-	struct program_header64 class64;
-};
+typedef union {
+	ProgramHeader32 class32;
+	ProgramHeader64 class64;
+} ProgramHeader;
 
-enum segment_type {
+typedef enum {
 	PT_LOAD    = 1,
 	PT_DYNAMIC = 2,
 	PT_INTERP  = 3
-};
+} SegmentType;
 
-struct dynamic_entry32 {
+typedef struct {
 	int32_t d_tag;
 	uint32_t d_val;
-};
+} DynamicEntry32;
 
-struct dynamic_entry64 {
+typedef struct {
 	int64_t d_tag;
 	uint64_t d_val;
-};
+} DynamicEntry64;
 
-union dynamic_entry {
-	struct dynamic_entry32 class32;
-	struct dynamic_entry64 class64;
-};
+typedef struct {
+	DynamicEntry32 class32;
+	DynamicEntry64 class64;
+} DynamicEntry;
 
-enum dynamic_type {
+typedef enum {
 	DT_STRTAB  = 5,
 	DT_RPATH   = 15,
 	DT_RUNPATH = 29
-};
+} DynamicType;
 
 /* The following macros are also compatible with ELF 64-bit. */
 #define ELF_IDENT(header, index) (header).class32.e_ident[(index)]
@@ -146,22 +146,20 @@ enum dynamic_type {
 	 : (dynent).class32. d_ ## field)
 
 #define KNOWN_PHENTSIZE(header, size)					\
-	(   (IS_CLASS32(header) && (size) == sizeof(struct program_header32)) \
-	 || (IS_CLASS64(header) && (size) == sizeof(struct program_header64)))
+	(   (IS_CLASS32(header) && (size) == sizeof(ProgramHeader32)) \
+	 || (IS_CLASS64(header) && (size) == sizeof(ProgramHeader64)))
 
 
 #include "tracee/tracee.h"
 
-extern int open_elf(const char *t_path, union elf_header *elf_header);
+extern int open_elf(const char *t_path, ElfHeader *elf_header);
 
-extern bool is_host_elf(const struct tracee *tracee, const char *t_path);
+extern bool is_host_elf(const Tracee *tracee, const char *t_path);
 
-extern int find_program_header(int fd,
-			const union elf_header *elf_header,
-			union program_header *program_header,
-			enum segment_type type, uint64_t address);
+extern int find_program_header(int fd, const ElfHeader *elf_header, ProgramHeader *program_header,
+			SegmentType type, uint64_t address);
 
-extern int read_ldso_rpaths(const struct tracee *tracee, int fd,
-			const union elf_header *elf_header, char **rpath, char **runpath);
+extern int read_ldso_rpaths(const Tracee *tracee, int fd, const ElfHeader *elf_header,
+			char **rpath, char **runpath);
 
 #endif /* ELF_H */

doc/stylesheets/cli.xsl
@@ -13,21 +13,21 @@
 #include "tracee/tracee.h"
 #include "build.h"
 
-struct argument {
+typedef struct {
 	const char *name;
 	char separator;
 	const char *value;
-};
+} Argument;
 
-typedef void (*option_handler_t)(struct tracee *tracee, char *value);
+typedef void (*option_handler_t)(Tracee *tracee, char *value);
 
-struct option {
+typedef struct {
 	const char *class;
 	option_handler_t handler;
 	const char *description;
 	const char *detail;
-	struct argument arguments[5];
-};
+	Argument arguments[5];
+} Option;
 
 #ifndef VERSION
 #define VERSION "</xsl:text><xsl:value-of select="//version" /><xsl:text>"
@@ -45,7 +45,7 @@ static const char *version = VERSION;
 
     <xsl:apply-templates select="//option_group" mode="handlers" />
     <xsl:text>
-static struct option options[] = {
+static Option options[] = {
 </xsl:text>
     <xsl:apply-templates select="//option_group" mode="options" />
   <xsl:text>};
@@ -166,7 +166,7 @@ static struct option options[] = {
   <xsl:template match="option_group" mode="handlers">
     <xsl:text>static void handle_option_</xsl:text>
     <xsl:value-of select="substring(option[1]/option_string, 2, 1)" />
-    <xsl:text>(struct tracee *tracee, char *value);
+    <xsl:text>(Tracee *tracee, char *value);
 </xsl:text>
   </xsl:template>
 

src/cli.c
@@ -46,26 +46,26 @@
 #include "execve/ldso.h"
 #include "build.h"
 
-static void handle_option_r(struct tracee *tracee, char *value)
+static void handle_option_r(Tracee *tracee, char *value)
 {
 	tracee->root = value;
 }
 
-struct binding {
+typedef struct binding {
 	const char *host;
 	const char *guest;
 	bool must_exist;
 
 	SLIST_ENTRY(binding) link;
-};
+} Binding;
 
 static SLIST_HEAD(bindings, binding) bindings;
 
 static void new_binding2(const char *host, const char *guest, bool must_exist)
 {
-	struct binding *binding;
+	Binding *binding;
 
-	binding = talloc_zero(NULL, struct binding);
+	binding = talloc_zero(NULL, Binding);
 	if (binding == NULL) {
 		notice(WARNING, INTERNAL, "talloc_zero() failed");
 		return;
@@ -93,7 +93,7 @@ static void new_binding(char *value, bool must_exist)
 	new_binding2(value, ptr, must_exist);
 }
 
-static void handle_option_b(struct tracee *tracee, char *value)
+static void handle_option_b(Tracee *tracee, char *value)
 {
 	new_binding(value, true);
 }
@@ -158,7 +158,7 @@ static void which(char *const command, char result[PATH_MAX])
 		notice(ERROR, SYSTEM, "realpath(\"%s\")", which_output);
 }
 
-static void handle_option_q(struct tracee *tracee, char *value)
+static void handle_option_q(Tracee *tracee, char *value)
 {
 	char path[PATH_MAX];
 	size_t nb_args;
@@ -222,22 +222,22 @@ static void handle_option_q(struct tracee *tracee, char *value)
 	new_binding2("/dev/null", "/etc/ld.so.preload", false);
 }
 
-static void handle_option_w(struct tracee *tracee, char *value)
+static void handle_option_w(Tracee *tracee, char *value)
 {
 	tracee->cwd = value;
 }
 
-static void handle_option_k(struct tracee *tracee, char *value)
+static void handle_option_k(Tracee *tracee, char *value)
 {
 	tracee->kernel_release = value;
 }
 
-static void handle_option_0(struct tracee *tracee, char *value)
+static void handle_option_0(Tracee *tracee, char *value)
 {
 	tracee->fake_id0 = true;
 }
 
-static void handle_option_v(struct tracee *tracee, char *value)
+static void handle_option_v(Tracee *tracee, char *value)
 {
 	char *end_ptr = NULL;
 
@@ -247,7 +247,7 @@ static void handle_option_v(struct tracee *tracee, char *value)
 		notice(ERROR, USER, "option `-v` expects an integer value.");
 }
 
-static void handle_option_V(struct tracee *tracee, char *value)
+static void handle_option_V(Tracee *tracee, char *value)
 {
 	printf("PRoot %s: %s.\n", version, subtitle);
 	printf("%s\n", colophon);
@@ -255,32 +255,32 @@ static void handle_option_V(struct tracee *tracee, char *value)
 }
 
 static void print_usage(bool);
-static void handle_option_h(struct tracee *tracee, char *value)
+static void handle_option_h(Tracee *tracee, char *value)
 {
 	print_usage(true);
 	exit(EXIT_SUCCESS);
 }
 
-static void handle_option_B(struct tracee *tracee, char *value)
+static void handle_option_B(Tracee *tracee, char *value)
 {
 	int i;
 	for (i = 0; recommended_bindings[i] != NULL; i++)
 		new_binding(recommended_bindings[i], false);
 }
 
-static void handle_option_Q(struct tracee *tracee, char *value)
+static void handle_option_Q(Tracee *tracee, char *value)
 {
 	handle_option_q(tracee, value);
 	handle_option_B(tracee, NULL);
 }
 
-static void handle_option_W(struct tracee *tracee, char *value)
+static void handle_option_W(Tracee *tracee, char *value)
 {
 	handle_option_w(tracee, ".");
 	handle_option_b(tracee, ".");
 }
 
-#define NB_OPTIONS (sizeof(options) / sizeof(struct option))
+#define NB_OPTIONS (sizeof(options) / sizeof(Option))
 
 /**
  * Print a (@detailed) usage of PRoot.
@@ -298,7 +298,7 @@ static void print_usage(bool detailed)
 
 	for (i = 0; i < NB_OPTIONS; i++) {
 		for (j = 0; ; j++) {
-			struct argument *argument = &(options[i].arguments[j]);
+			Argument *argument = &(options[i].arguments[j]);
 
 			if (!argument->name || (!detailed && j != 0)) {
 				DETAIL(printf("\n"));
@@ -345,7 +345,7 @@ static void print_execve_help(const char *argv0)
 "  * <qemu> does not work correctly (if specified).");
 }
 
-static void error_separator(struct argument *argument)
+static void error_separator(Argument *argument)
 {
 	if (argument->separator == '\0')
 		notice(ERROR, USER,
@@ -384,7 +384,7 @@ static void print_argv(const char *prompt, char **argv)
 	notice(INFO, USER, "%s", string);
 }
 
-static void print_config(const struct tracee *tracee)
+static void print_config(const Tracee *tracee)
 {
 	notice(INFO, USER, "guest rootfs = %s", tracee->root);
 
@@ -415,10 +415,10 @@ static void print_config(const struct tracee *tracee)
  * Configure @tracee according to the command-line arguments stored in
  * @argv[].  This function succeed or die trying.
  */
-static void parse_cli(struct tracee *tracee, int argc, char *argv[])
+static void parse_cli(Tracee *tracee, int argc, char *argv[])
 {
 	option_handler_t handler = NULL;
-	struct binding *binding;
+	Binding *binding;
 	char tmp[PATH_MAX];
 	int i, j, k;
 	int status;
@@ -442,11 +442,11 @@ static void parse_cli(struct tracee *tracee, int argc, char *argv[])
 			break; /* End of PRoot options. */
 
 		for (j = 0; j < NB_OPTIONS; j++) {
-			struct option *option = &options[j];
+			Option *option = &options[j];
 
 			/* A given option has several aliases.  */
 			for (k = 0; ; k++) {
-				struct argument *argument;
+				Argument *argument;
 				size_t length;
 
 				argument = &option->arguments[k];
@@ -524,7 +524,7 @@ static void parse_cli(struct tracee *tracee, int argc, char *argv[])
 
 	binding = SLIST_FIRST(&bindings);
 	while (binding != NULL) {
-		struct binding *next = SLIST_NEXT(binding, link);
+		Binding *next = SLIST_NEXT(binding, link);
 		bind_path(tracee, binding->host, binding->guest, binding->must_exist);
 		TALLOC_FREE(binding);
 		binding = next;
@@ -537,12 +537,11 @@ static void parse_cli(struct tracee *tracee, int argc, char *argv[])
 
 	if (verbose_level > 0)
 		print_config(tracee);
-
 }
 
 int main(int argc, char *argv[])
 {
-	struct tracee *tracee;
+	Tracee *tracee;
 	int status;
 
 	/* Configure the memory allocator.  */

src/cli.h
@@ -7,21 +7,21 @@
 #include "tracee/tracee.h"
 #include "build.h"
 
-struct argument {
+typedef struct {
 	const char *name;
 	char separator;
 	const char *value;
-};
+} Argument;
 
-typedef void (*option_handler_t)(struct tracee *tracee, char *value);
+typedef void (*option_handler_t)(Tracee *tracee, char *value);
 
-struct option {
+typedef struct {
 	const char *class;
 	option_handler_t handler;
 	const char *description;
 	const char *detail;
-	struct argument arguments[5];
-};
+	Argument arguments[5];
+} Option;
 
 #ifndef VERSION
 #define VERSION "2.2"
@@ -53,20 +53,20 @@ static char *recommended_bindings[] = {
 	NULL,
 };
 
-static void handle_option_r(struct tracee *tracee, char *value);
-static void handle_option_b(struct tracee *tracee, char *value);
-static void handle_option_q(struct tracee *tracee, char *value);
-static void handle_option_w(struct tracee *tracee, char *value);
-static void handle_option_k(struct tracee *tracee, char *value);
-static void handle_option_0(struct tracee *tracee, char *value);
-static void handle_option_v(struct tracee *tracee, char *value);
-static void handle_option_V(struct tracee *tracee, char *value);
-static void handle_option_h(struct tracee *tracee, char *value);
-static void handle_option_B(struct tracee *tracee, char *value);
-static void handle_option_Q(struct tracee *tracee, char *value);
-static void handle_option_W(struct tracee *tracee, char *value);
+static void handle_option_r(Tracee *tracee, char *value);
+static void handle_option_b(Tracee *tracee, char *value);
+static void handle_option_q(Tracee *tracee, char *value);
+static void handle_option_w(Tracee *tracee, char *value);
+static void handle_option_k(Tracee *tracee, char *value);
+static void handle_option_0(Tracee *tracee, char *value);
+static void handle_option_v(Tracee *tracee, char *value);
+static void handle_option_V(Tracee *tracee, char *value);
+static void handle_option_h(Tracee *tracee, char *value);
+static void handle_option_B(Tracee *tracee, char *value);
+static void handle_option_Q(Tracee *tracee, char *value);
+static void handle_option_W(Tracee *tracee, char *value);
 
-static struct option options[] = {
+static Option options[] = {
 	{ .class = "Regular options",
 	  .arguments = {
 		{ .name = "-r", .separator = ' ', .value = "path" },

src/execve/elf.c
@@ -42,7 +42,7 @@
  * This function returns -errno if an error occured, otherwise the
  * file descriptor for @t_path.
  */
-int open_elf(const char *t_path, union elf_header *elf_header)
+int open_elf(const char *t_path, ElfHeader *elf_header)
 {
 	int fd;
 	int status;
@@ -56,8 +56,8 @@ int open_elf(const char *t_path, union elf_header *elf_header)
 		return -errno;
 
 	/* Check if it is an ELF file.  */
-	status = read(fd, elf_header, sizeof(union elf_header));
-	if (status < sizeof(union elf_header)
+	status = read(fd, elf_header, sizeof(ElfHeader));
+	if (status < sizeof(ElfHeader)
 	    || ELF_IDENT(*elf_header, 0) != 0x7f
 	    || ELF_IDENT(*elf_header, 1) != 'E'
 	    || ELF_IDENT(*elf_header, 2) != 'L'
@@ -91,10 +91,8 @@ end:
  * returns -errno if an error occured, 1 if the program header was
  * found, otherwise 0.
  */
-int find_program_header(int fd,
-			const union elf_header *elf_header,
-			union program_header *program_header,
-			enum segment_type type, uint64_t address)
+int find_program_header(int fd, const ElfHeader *elf_header, ProgramHeader *program_header,
+			SegmentType type, uint64_t address)
 {
 	uint64_t elf_phoff;
 	uint16_t elf_phentsize;
@@ -160,11 +158,11 @@ int find_program_header(int fd,
 /**
  * Check if @t_path is an ELF file for the host architecture.
  */
-bool is_host_elf(const struct tracee *tracee, const char *t_path)
+bool is_host_elf(const Tracee *tracee, const char *t_path)
 {
 	int host_elf_machine[] = HOST_ELF_MACHINE;
 	static int force_foreign = -1;
-	union elf_header elf_header;
+	ElfHeader elf_header;
 	uint16_t elf_machine;
 	int fd;
 	int i;
@@ -198,12 +196,11 @@ bool is_host_elf(const struct tracee *tracee, const char *t_path)
  * if @callback returns an error (something < 0).  This function
  * returns -errno if an error occured, otherwise 0.
  */
-static int foreach_dynamic_entry(int fd,
-			const union elf_header *elf_header,
-			const union program_header *program_header,
-			enum dynamic_type type, int (*callback)(uint64_t))
+static int foreach_dynamic_entry(int fd, const ElfHeader *elf_header,
+				const ProgramHeader *program_header,
+				DynamicType type, int (*callback)(uint64_t))
 {
-	union dynamic_entry dynamic_entry;
+	DynamicEntry dynamic_entry;
 	size_t sizeof_dynamic_entry;
 	uint64_t offset;
 	uint64_t size;
@@ -219,9 +216,9 @@ static int foreach_dynamic_entry(int fd,
 	size   = PROGRAM_FIELD(*elf_header, *program_header, filesz);
 
 	if (IS_CLASS32(*elf_header))
-		sizeof_dynamic_entry = sizeof(struct dynamic_entry32);
+		sizeof_dynamic_entry = sizeof(DynamicEntry32);
 	else
-		sizeof_dynamic_entry = sizeof(struct dynamic_entry64);
+		sizeof_dynamic_entry = sizeof(DynamicEntry64);
 
 	if (size % sizeof_dynamic_entry != 0)
 		return -ENOEXEC;
@@ -254,7 +251,7 @@ static int foreach_dynamic_entry(int fd,
  * referenced by @fd at the given @offset.  This function returns
  * -errno if an error occured, otherwise 0.
  */
-static int add_xpaths(const struct tracee *tracee, int fd, uint64_t offset, char **xpaths)
+static int add_xpaths(const Tracee *tracee, int fd, uint64_t offset, char **xpaths)
 {
 	char *paths = NULL;
 	char *tmp;
@@ -317,11 +314,11 @@ static int add_xpaths(const struct tracee *tracee, int fd, uint64_t offset, char
  * @runpaths respectively.  This function returns -errno if an error
  * occured, otherwise 0.
  */
-int read_ldso_rpaths(const struct tracee* tracee, int fd,
-		const union elf_header *elf_header, char **rpaths, char **runpaths)
+int read_ldso_rpaths(const Tracee* tracee, int fd, const ElfHeader *elf_header,
+		char **rpaths, char **runpaths)
 {
-	union program_header dynamic;
-	union program_header strtab_segment;
+	ProgramHeader dynamic;
+	ProgramHeader strtab_segment;
 	uint64_t strtab_address = (uint64_t) -1;
 	off_t strtab_offset;
 	int status;

src/execve/execve.c
@@ -43,7 +43,7 @@
  * executable and is a regular file.  This function returns -errno if
  * an error occured, 0 otherwise.
  */
-static int translate_n_check(struct tracee *tracee, char t_path[PATH_MAX], const char *u_path)
+static int translate_n_check(Tracee *tracee, char t_path[PATH_MAX], const char *u_path)
 {
 	struct stat statl;
 	int status;
@@ -73,13 +73,9 @@ static int translate_n_check(struct tracee *tracee, char t_path[PATH_MAX], const
  * the program itself if it doesn't use an interpreter) are stored in
  * @t_interp and @u_interp (respectively translated and untranslated).
  */
-static int expand_interp(struct tracee *tracee,
-			 const char *u_path,
-			 char t_interp[PATH_MAX],
-			 char u_interp[PATH_MAX],
-			 struct array *argv,
-			 extract_interp_t callback,
-			 bool ignore_interpreter)
+static int expand_interp(Tracee *tracee, const char *u_path, char t_interp[PATH_MAX],
+			char u_interp[PATH_MAX], Array *argv, extract_interp_t callback,
+			bool ignore_interpreter)
 {
 	char argument[ARG_MAX];
 
@@ -201,14 +197,14 @@ static int expand_interp(struct tracee *tracee,
  *
  *     execve("/tmp/new_root/bin/sh", argv = [ "/bin/sh", "/bin/script.sh", "arg1", arg2", ... ], envp);
  */
-int translate_execve(struct tracee *tracee)
+int translate_execve(Tracee *tracee)
 {
 	char u_path[PATH_MAX];
 	char t_interp[PATH_MAX];
 	char u_interp[PATH_MAX];
 
-	struct array *envp = NULL;
-	struct array *argv = NULL;
+	Array *envp = NULL;
+	Array *argv = NULL;
 	char *argv0 = NULL;
 
 	bool ignore_elf_interpreter;
@@ -222,7 +218,7 @@ int translate_execve(struct tracee *tracee)
 	if (status < 0)
 		return status;
 
-	argv = talloc_zero(tracee->tmp, struct array);
+	argv = talloc_zero(tracee->tmp, Array);
 	if (argv == NULL)
 		return -ENOMEM;
 
@@ -240,7 +236,7 @@ int translate_execve(struct tracee *tracee)
 		if (argv0 != NULL)
 			argv0 = talloc_strdup(tracee->tmp, argv0);
 
-		envp = talloc_zero(tracee->tmp, struct array);
+		envp = talloc_zero(tracee->tmp, Array);
 		if (envp == NULL)
 			return -ENOMEM;
 

src/execve/execve.h
@@ -26,6 +26,6 @@
 #include "tracee/tracee.h"
 #include "tracee/array.h"
 
-extern int translate_execve(struct tracee *tracee);
+extern int translate_execve(Tracee *tracee);
 
 #endif /* EXECVE_H */

src/execve/interp.c
@@ -43,10 +43,8 @@
  *     passed as a *single* argument to the interpreter, and this
  *     string can include white space.
  */
-int extract_script_interp(const struct tracee *tracee,
-			  const char *t_path,
-			  char u_interp[PATH_MAX],
-			  char argument[ARG_MAX])
+int extract_script_interp(const Tracee *tracee, const char *t_path,
+			char u_interp[PATH_MAX], char argument[ARG_MAX])
 {
 	char tmp;
 
@@ -184,13 +182,11 @@ end:
  * returns -errno if an error occured, 1 if a ELF interpreter was
  * found and extracted, otherwise 0.
  */
-int extract_elf_interp(const struct tracee *tracee,
-		       const char *t_path,
-		       char u_interp[PATH_MAX],
-		       char argument[ARG_MAX])
+int extract_elf_interp(const Tracee *tracee, const char *t_path,
+		       char u_interp[PATH_MAX], char argument[ARG_MAX])
 {
-	union elf_header elf_header;
-	union program_header program_header;
+	ElfHeader elf_header;
+	ProgramHeader program_header;
 
 	size_t extra_size;
 	int status;

commit 31b10eb0d485f607a0d44b0c843aa3a2303179a7

Fix a regression introduced in commit 294e958.

src/execve/elf.h
@@ -110,7 +110,7 @@ typedef struct {
 	uint64_t d_val;
 } DynamicEntry64;
 
-typedef struct {
+typedef union {
 	DynamicEntry32 class32;
 	DynamicEntry64 class64;
 } DynamicEntry;
@@ -127,19 +127,19 @@ typedef enum {
 #define IS_CLASS32(header) (ELF_CLASS(header) == 1)
 #define IS_CLASS64(header) (ELF_CLASS(header) == 2)
 
-/* Helper to access a @field of the structure elf_headerXX. */
+/* Helper to access a @field of the structure ElfHeaderXX. */
 #define ELF_FIELD(header, field)		\
 	(IS_CLASS64(header)			\
 	 ? (header).class64. e_ ## field	\
 	 : (header).class32. e_ ## field)
 
-/* Helper to access a @field of the structure program_headerXX */
+/* Helper to access a @field of the structure ProgramHeaderXX */
 #define PROGRAM_FIELD(ehdr, phdr, field)	\
 	(IS_CLASS64(ehdr)			\
 	 ? (phdr).class64. p_ ## field		\
 	 : (phdr).class32. p_ ## field)
 
-/* Helper to access a @field of the structure dynamic_entryXX */
+/* Helper to access a @field of the structure DynamicEntryXX */
 #define DYNAMIC_FIELD(ehdr, dynent, field)	\
 	(IS_CLASS64(ehdr)			\
 	 ? (dynent).class64. d_ ## field	\