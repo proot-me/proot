proot-me/proot

commit b0c5428024dfb7059afb221f7b4d68137e7cd058

Use a unique Binding structure + move the binding lists into the Tracee structure.
Now everything but the verbose level can be configured on a per tracee
basis.

Change-Id: Ie146a80582b1543facd9f7e71cc7817db8930063

src/path/binding.c
@@ -26,6 +26,7 @@
 #include <unistd.h>   /* getcwd(2), lstat(2), mknod(2), symlink(2), */
 #include <stdlib.h>   /* realpath(3), mkdtemp(3), */
 #include <string.h>   /* string(3),  */
+#include <strings.h>  /* bzero(3), */
 #include <assert.h>   /* assert(3), */
 #include <limits.h>   /* PATH_MAX, */
 #include <errno.h>    /* errno, E* */
@@ -38,28 +39,12 @@
 #include "path/canon.h"
 #include "notice.h"
 
-typedef struct binding {
-	Path host;
-	Path guest;
+#define HEAD(tracee, side)  (side == GUEST ? (tracee)->bindings_guest : (tracee)->bindings_host)
 
-	bool need_substitution;
-
-	LIST_ENTRY(binding) guest_link;
-	LIST_ENTRY(binding) host_link;
-} Binding;
-
-typedef LIST_HEAD(bindings, binding) Bindings;
-
-static Bindings bindings_guest;
-static Bindings bindings_host;
-
-#define FIELD(side) (side == GUEST ? guest_link : host_link)
-#define HEAD(side)  (side == GUEST ? &bindings_guest : &bindings_host)
-
-#define LIST_FOREACH_(binding, side)					\
-	for (binding = LIST_FIRST(HEAD(side));				\
+#define LIST_FOREACH_(tracee, binding, side)				\
+	for (binding = LIST_FIRST(HEAD(tracee, side));			\
 	     binding != NULL;						\
-	     binding = (side == GUEST				\
+	     binding = (side == GUEST					\
 		     ? LIST_NEXT(binding, guest_link)			\
 		     : LIST_NEXT(binding, host_link)))
 
@@ -77,21 +62,32 @@ static Bindings bindings_host;
 		LIST_INSERT_BEFORE(next, binding, host_link);	\
 } while (0)
 
-#define LIST_INSERT_HEAD_(binding, side) do {			\
+#define LIST_INSERT_HEAD_(tracee, binding, side) do {		\
 	if (side == GUEST)					\
-		LIST_INSERT_HEAD(HEAD(side), binding, guest_link); \
+		LIST_INSERT_HEAD(HEAD(tracee, side), binding, guest_link); \
 	else							\
-		LIST_INSERT_HEAD(HEAD(side), binding, host_link); \
+		LIST_INSERT_HEAD(HEAD(tracee, side), binding, host_link); \
+} while (0)
+
+#define IS_LINKED(binding, link)					\
+	((binding)->link.le_next != NULL || (binding)->link.le_prev != NULL)
+
+#define LIST_REMOVE_(binding, link) do {		\
+	LIST_REMOVE(binding, link);			\
+	bzero(&binding->link, sizeof(binding->link));	\
 } while (0)
 
 /**
  * Print all bindings (verbose purpose).
  */
-void print_bindings()
+static void print_bindings(const Tracee *tracee)
 {
 	const Binding *binding;
 
-	LIST_FOREACH_(binding, GUEST) {
+	if (tracee->bindings_guest == NULL)
+		return;
+
+	LIST_FOREACH_(tracee, binding, GUEST) {
 		if (compare_paths(binding->host.path, binding->guest.path) == PATHS_ARE_EQUAL)
 			notice(INFO, USER, "binding = %s", binding->host.path);
 		else
@@ -104,7 +100,7 @@ void print_bindings()
  * Get the binding for the given @path (relatively to the given
  * binding @side).
  */
-static const Binding *get_binding(const Tracee *tracee, Side side, const char path[PATH_MAX])
+static const Binding *get_binding(Tracee *tracee, Side side, const char path[PATH_MAX])
 {
 	const Binding *binding;
 	size_t path_length = strlen(path);
@@ -112,7 +108,7 @@ static const Binding *get_binding(const Tracee *tracee, Side side, const char pa
 	/* Sanity checks.  */
 	assert(path != NULL && path[0] == '/');
 
-	LIST_FOREACH_(binding, side) {
+	LIST_FOREACH_(tracee, binding, side) {
 		Comparison comparison;
 		const Path *ref;
 
@@ -155,7 +151,7 @@ static const Binding *get_binding(const Tracee *tracee, Side side, const char pa
  * Get the binding path for the given @path (relatively to the given
  * binding @side).
  */
-const char *get_path_binding(const Tracee *tracee, Side side, const char path[PATH_MAX])
+const char *get_path_binding(Tracee *tracee, Side side, const char path[PATH_MAX])
 {
 	const Binding *binding;
 
@@ -188,7 +184,7 @@ const char *get_path_binding(const Tracee *tracee, Side side, const char path[PA
  *     * 1 if it is a binding location and a substitution was performed
  *       ("asymmetric" binding)
  */
-int substitute_binding(const Tracee *tracee, Side side, char path[PATH_MAX])
+int substitute_binding(Tracee *tracee, Side side, char path[PATH_MAX])
 {
 	const Path *reverse_ref;
 	const Path *ref;
@@ -278,14 +274,14 @@ int substitute_binding(const Tracee *tracee, Side side, char path[PATH_MAX])
  *
  * Note: "nested" from the @side point-of-view.
  */
-static void insort_binding(Side side, Binding *binding)
+static void insort_binding(const Tracee *tracee, Side side, Binding *binding)
 {
 	Binding *iterator;
 	Binding *previous = NULL;
-	Binding *next = LIST_FIRST(HEAD(side));
+	Binding *next = LIST_FIRST(HEAD(tracee, side));
 
 	/* Find where it should be added in the list.  */
-	LIST_FOREACH_(iterator, side) {
+	LIST_FOREACH_(tracee, iterator, side) {
 		Comparison comparison;
 		const Path *binding_path;
 		const Path *iterator_path;
@@ -348,21 +344,27 @@ static void insort_binding(Side side, Binding *binding)
 	else if (next != NULL)
 		LIST_INSERT_BEFORE_(next, binding, side);
 	else
-		LIST_INSERT_HEAD_(binding, side);
+		LIST_INSERT_HEAD_(tracee, binding, side);
+
+	/* Declare tracee->bindings_side as a parent of this
+	 * binding.  */
+	(void) talloc_reference(HEAD(tracee, side), binding);
 }
 
 /**
  * c.f. function above.
  */
-static void insort_binding2(Binding *binding)
+static void insort_binding2(Tracee *tracee, Binding *binding)
 {
-	insort_binding(GUEST, binding);
-	insort_binding(HOST, binding);
+	insort_binding(tracee, GUEST, binding);
+	insort_binding(tracee, HOST, binding);
 }
 
 /**
  * Delete only empty files and directories from the glue: the files
  * created by the user inside this glue are left.
+ *
+ * Note: this is a Talloc desctructor.
  */
 static int remove_glue(char *path)
 {
@@ -460,7 +462,7 @@ mode_t build_glue(Tracee *tracee, const char *guest_path, char host_path[PATH_MA
 
 	/* From the example, create the binding "/black" ->
 	 * "$GLUE/black".  */
-	binding = talloc_zero(NULL, Binding);
+	binding = talloc_zero(tracee->glue, Binding);
 	if (!binding) {
 		notice(WARNING, INTERNAL, "talloc_zero() failed");
 		return 0;
@@ -474,7 +476,7 @@ mode_t build_glue(Tracee *tracee, const char *guest_path, char host_path[PATH_MA
 
 	binding->need_substitution = true;
 
-	insort_binding2(binding);
+	insort_binding2(tracee, binding);
 
 	/* TODO: emulation of getdents(parent(guest_path)) to finalize
 	 * the glue, "black" in getdent("/") from the example.  */
@@ -483,73 +485,131 @@ mode_t build_glue(Tracee *tracee, const char *guest_path, char host_path[PATH_MA
 }
 
 /**
- * Free all the bindings.
+ * Remove @binding from all the list of bindings it belongs to.
+ *
+ * Note: this is a Talloc destructor.
  */
-void free_bindings()
+static int remove_binding(Binding *binding)
 {
-	Binding *binding = LIST_FIRST(HEAD(GUEST));
+	if (IS_LINKED(binding, user_link))
+		LIST_REMOVE_(binding, user_link);
 
-	while (binding != NULL) {
-		Binding *next = LIST_NEXT(binding, guest_link);
-		TALLOC_FREE(binding);
-		binding = next;
-	}
+	if (IS_LINKED(binding, guest_link))
+		LIST_REMOVE_(binding, guest_link);
+
+	if (IS_LINKED(binding, host_link))
+		LIST_REMOVE_(binding, host_link);
+
+	return 0;
 }
 
 /**
- * Save @host_path:@guest_path in the list of bindings for the
- * translation mechanism.  This function returns -1 if an error
- * occured, 0 otherwise.
+ * Allocate a new binding "@host:@guest" and attach it to
+ * @tracee->bindings_user.  This function complains about missing
+ * @host path only if @must_exist is true.  This function returns the
+ * allocated binding on success, NULL on error.
  */
-int bind_path(Tracee *tracee, const char *host_path, const char *guest_path, bool must_exist)
+Binding *new_binding(Tracee *tracee, const char *host, const char *guest, bool must_exist)
 {
 	Binding *binding;
-	int status;
 
-	assert(host_path != NULL);
+	/* Should be called only from the CLI support.  */
+	assert(tracee->pid == 0);
 
-	binding = talloc_zero(NULL, Binding);
-	if (binding == NULL) {
-		notice(WARNING, INTERNAL, "talloc_zero() failed");
-		goto error;
+	/* Lasy allocation of the list of bindings specified by the
+	 * user.  This list will be used by initialize_bindings().  */
+	if (tracee->bindings_user == NULL) {
+		tracee->bindings_user = talloc_zero(tracee, Bindings);
+		if (tracee->bindings_user == NULL) {
+			notice(WARNING, INTERNAL, "talloc_zero() failed");
+			return NULL;
+		}
 	}
 
-	if (realpath(host_path, binding->host.path) == NULL) {
+	/* Allocate an empty binding.  */
+	binding = talloc_zero(tracee->bindings_user, Binding);
+	if (binding == NULL) {
+		notice(WARNING, INTERNAL, "talloc_zero() failed");
+		return NULL;
+	}
+	talloc_set_destructor(binding, remove_binding);
+
+	/* Expand environment variables like $HOME.  */
+	if (host[0] == '$' && getenv(&host[1]))
+		host = getenv(&host[1]);
+
+	/* Canonicalize the host part of the binding, as expected by
+	 * get_binding().  */
+	if (realpath(host, binding->host.path) == NULL) {
 		if (must_exist)
-			notice(WARNING, SYSTEM, "realpath(\"%s\")", host_path);
+			notice(WARNING, SYSTEM, "realpath(\"%s\")", host);
 		goto error;
 	}
 	binding->host.length = strlen(binding->host.path);
 
-	/* Initial state before canonicalization.  */
-	strcpy(binding->guest.path, "/");
+	/* Symetric binding?  */
+	guest = guest ?: host;
 
-	if (guest_path != NULL && compare_paths(guest_path, "/") == PATHS_ARE_EQUAL) {
-		if (LIST_FIRST(HEAD(HOST)) != NULL)
-			notice(ERROR, USER,
-				"explicit binding to \"/\" isn't allowed, "
-				"use the -r option instead");
+	/* Save the expected guest part, it will be canonicalized by
+	 * initialize_bindings().  */
+	binding->guest.length = strlen(guest);
+	if (binding->guest.length >= PATH_MAX) {
+		notice(WARNING, USER, "path to the rootfs is too long (\"%s\")", guest);
+		goto error;
+	}
+	strcpy(binding->guest.path, guest);
+
+	/* The binding to "/" has to be installed before other
+	 * bindings since this former is required to canonicalize
+	 * these latters (c.f. initialize_binding() for details).  */
+	if (compare_paths(binding->guest.path, "/") == PATHS_ARE_EQUAL
+	    || LIST_FIRST(tracee->bindings_user) == NULL)
+		LIST_INSERT_HEAD(tracee->bindings_user, binding, user_link);
+	else
+		LIST_INSERT_AFTER(LIST_FIRST(tracee->bindings_user), binding, user_link);
+	return binding;
+
+error:
+	TALLOC_FREE(binding);
+	return NULL;
+}
+
+/**
+ * Canonicalize the guest part of the given @binding, insert it into
+ * @tracee->bindings_guest and @tracee->bindings_host, then remove it
+ * from @tracee->bindings_user.  This function returns -1 if an error
+ * occured, 0 otherwise.
+ */
+static int initialize_binding(Tracee *tracee, Binding *binding)
+{
+	char path[PATH_MAX];
+	struct stat statl;
+	int status;
+
+	if (compare_paths(binding->guest.path, "/") == PATHS_ARE_EQUAL) {
+		if (LIST_FIRST(HEAD(tracee, HOST)) != NULL)
+			notice(ERROR, USER, "explicit binding to \"/\" isn't allowed, "
+					    "use the -r option instead");
+		strcpy(binding->guest.path, "/");
 	}
 	else {
-		struct stat statl;
-		char path[PATH_MAX];
-
-		/* Symetric binding?  */
-		guest_path = guest_path ?: host_path;
-
 		/* When not absolute, assume the guest_path is
 		 * relative to the host cwd (eg. ``-b .``).  */
-		if (guest_path[0] != '/') {
+		if (binding->guest.path[0] != '/') {
 			char cwd[PATH_MAX];
+
 			if (getcwd(cwd, PATH_MAX) == NULL) {
 				notice(WARNING, SYSTEM, "can't sanitize binding \"%s\", getcwd()",
 					binding->guest.path);
 				goto error;
 			}
-			join_paths(2, path, cwd, guest_path);
+			join_paths(2, path, cwd, binding->guest.path);
 		}
 		else
-			strcpy(path, guest_path);
+			strcpy(path, binding->guest.path);
+
+		/* Initial state before canonicalization.  */
+		strcpy(binding->guest.path, "/");
 
 		/* Remember the type of the final component, it will be used
 		 * in build_glue() later.  */
@@ -587,10 +647,54 @@ int bind_path(Tracee *tracee, const char *host_path, const char *guest_path, boo
 		binding->guest.length -= 1;
 	}
 
-	insort_binding2(binding);
+	insort_binding2(tracee, binding);
 	return 0;
 
 error:
 	TALLOC_FREE(binding);
 	return -1;
+}
+
+/**
+ * Allocate @tracee->bindings_guest and @tracee->bindings_host, then
+ * call initialize_binding() on each binding listed in
+ * @tracee->bindings_user.
+ */
+void initialize_bindings(Tracee *tracee)
+{
+	Binding *binding;
+
+	/* Sanity checks.  */
+	assert(    tracee->bindings_guest == NULL
+		&& tracee->bindings_host  == NULL
+		&& tracee->bindings_user  != NULL);
+
+	/* Allocate @tracee->bindings_guest and
+	 * @tracee->bindings_host.  */
+	tracee->bindings_guest = talloc_zero(tracee, Bindings);
+	tracee->bindings_host  = talloc_zero(tracee, Bindings);
+	if (tracee->bindings_guest == NULL || tracee->bindings_host == NULL)
+		notice(ERROR, INTERNAL, "can't allocate enough memory");
+
+	/* Sanity check: the first binding has to be "/".  */
+	binding = LIST_FIRST(tracee->bindings_user);
+	assert(compare_paths(binding->guest.path, "/") == PATHS_ARE_EQUAL);
+
+	/* Call initialize_binding() on each binding listed in
+	 * @tracee->bindings_user.  */
+	while (binding != NULL) {
+		Binding *next;
+
+		next = LIST_NEXT(binding, user_link);
+
+		initialize_binding(tracee, binding);
+
+		LIST_REMOVE_(binding, user_link);
+		binding = next;
+	}
+
+	TALLOC_FREE(tracee->bindings_user);
+
+	if (verbose_level > 0)
+		print_bindings(tracee);
 }

src/cli.c
@@ -33,13 +33,14 @@
 #include <unistd.h>    /* stat(2), */
 #include <errno.h>     /* errno(3), */
 #include <linux/limits.h> /* PATH_MAX, ARG_MAX, */
-#include <sys/queue.h> /* SLIST_*, */
+#include <sys/queue.h> /* LIST_*, */
 #include <talloc.h>    /* talloc_*, */
 
 #include "cli.h"
 #include "path/binding.h"
 #include "notice.h"
 #include "path/path.h"
+#include "path/binding.h"
 #include "tracee/tracee.h"
 #include "tracee/event.h"
 #include "tracee/tracee.h"
@@ -51,34 +52,7 @@ static void handle_option_r(Tracee *tracee, char *value)
 	tracee->root = value;
 }
 
-typedef struct binding {
-	const char *host;
-	const char *guest;
-	bool must_exist;
-
-	SLIST_ENTRY(binding) link;
-} Binding;
-
-static SLIST_HEAD(bindings, binding) bindings;
-
-static void new_binding2(const char *host, const char *guest, bool must_exist)
-{
-	Binding *binding;
-
-	binding = talloc_zero(NULL, Binding);
-	if (binding == NULL) {
-		notice(WARNING, INTERNAL, "talloc_zero() failed");
-		return;
-	}
-
-	binding->host  = host;
-	binding->guest = guest;
-	binding->must_exist = must_exist;
-
-	SLIST_INSERT_HEAD(&bindings, binding, link);
-}
-
-static void new_binding(char *value, bool must_exist)
+static void handle_option_b(Tracee *tracee, char *value)
 {
 	char *ptr = strchr(value, ':');
 	if (ptr != NULL) {
@@ -86,16 +60,7 @@ static void new_binding(char *value, bool must_exist)
 		ptr++;
 	}
 
-	/* Expand environment variables like $HOME.  */
-	if (value[0] == '$' && getenv(&value[1]))
-		value = getenv(&value[1]);
-
-	new_binding2(value, ptr, must_exist);
-}
-
-static void handle_option_b(Tracee *tracee, char *value)
-{
-	new_binding(value, true);
+	new_binding(tracee, value, ptr, true);
 }
 
 /**
@@ -219,8 +184,8 @@ static void handle_option_q(Tracee *tracee, char *value)
 	which(tracee->qemu[0], path);
 	tracee->qemu[0] = talloc_strdup(tracee->qemu, path);
 
-	new_binding2("/", HOST_ROOTFS, true);
-	new_binding2("/dev/null", "/etc/ld.so.preload", false);
+	new_binding(tracee, "/", HOST_ROOTFS, true);
+	new_binding(tracee, "/dev/null", "/etc/ld.so.preload", false);
 }
 
 static void handle_option_w(Tracee *tracee, char *value)
@@ -267,7 +232,7 @@ static void handle_option_B(Tracee *tracee, char *value)
 {
 	int i;
 	for (i = 0; recommended_bindings[i] != NULL; i++)
-		new_binding(recommended_bindings[i], false);
+		new_binding(tracee, recommended_bindings[i], NULL, false);
 }
 
 static void handle_option_Q(Tracee *tracee, char *value)
@@ -403,8 +368,6 @@ static void print_config(const Tracee *tracee)
 		notice(INFO, USER, "fake root id = true");
 
 	notice(INFO, USER, "verbose level = %d", verbose_level);
-
-	print_bindings();
 }
 
 /**
@@ -415,7 +378,6 @@ static void parse_cli(Tracee *tracee, int argc, char *argv[])
 {
 	option_handler_t handler = NULL;
 	Binding *binding;
-	char tmp[PATH_MAX];
 	int i, j, k;
 	int status;
 
@@ -507,25 +469,22 @@ static void parse_cli(Tracee *tracee, int argc, char *argv[])
 			tracee->root = "/";
 	}
 
-	if (realpath(tracee->root, tmp) == NULL)
-		notice(ERROR, SYSTEM, "realpath(\"%s\")", tracee->root);
-	tracee->root = talloc_strdup(tracee, tmp);
+	/* Note: ``chroot $PATH`` is almost equivalent to ``mount
+	 * --bind $PATH /``.  PRoot just ensures it is inserted before
+	 * other bindings since this former is required to
+	 * canonicalize these latters.  */
+	binding = new_binding(tracee, tracee->root, "/", true);
+	if (binding == NULL)
+		notice(ERROR, SYSTEM, "fatal!");
+
+	/* tracee->root is mainly used to avoid costly lookups in
+	 * tracee->bindings_guest.  */
+	tracee->root = talloc_strdup(tracee, binding->host.path);
 	talloc_set_name_const(tracee->root, "$root");
 
-	/* The binding to "/" has to be initialized before other
-	 * bindings since this former is required to sanitize these
-	 * latters (c.f. bind_path() for details).  */
-	status = bind_path(tracee, tracee->root, "/", true);
-	if (status < 0)
-		notice(ERROR, USER, "fatal");
-
-	binding = SLIST_FIRST(&bindings);
-	while (binding != NULL) {
-		Binding *next = SLIST_NEXT(binding, link);
-		bind_path(tracee, binding->host, binding->guest, binding->must_exist);
-		TALLOC_FREE(binding);
-		binding = next;
-	}
+	/* Bindings specify by the user (tracee->bindings_user) can
+	 * now be canonicalized, as expected by get_bindings().  */
+	initialize_bindings(tracee);
 
 	if (i < argc)
 		tracee->cmdline = &argv[i];

src/path/binding.h
@@ -28,12 +28,24 @@
 
 #include "path.h"
 
-extern int bind_path(Tracee *tracee, const char *host_path, const char *guest_path, bool must_exist);
-extern mode_t build_glue(Tracee *tracee, const char *guest_path, char host_path[PATH_MAX], Finality is_final);
-extern void print_bindings(void);
-extern void free_bindings(void);
+typedef struct binding {
+	Path host;
+	Path guest;
 
-extern const char *get_path_binding(const Tracee* tracee, Side side, const char path[PATH_MAX]);
-extern int substitute_binding(const Tracee* tracee, Side side, char path[PATH_MAX]);
+	bool need_substitution;
+	bool must_exist;
+
+	LIST_ENTRY(binding) user_link;
+	LIST_ENTRY(binding) guest_link;
+	LIST_ENTRY(binding) host_link;
+} Binding;
+
+typedef LIST_HEAD(bindings, binding) Bindings;
+
+extern mode_t build_glue(Tracee *tracee, const char *guest_path, char host_path[PATH_MAX], Finality is_final);
+extern Binding *new_binding(Tracee *tracee, const char *host, const char *guest, bool must_exist);
+extern void initialize_bindings(Tracee *tracee);
+extern const char *get_path_binding(Tracee* tracee, Side side, const char path[PATH_MAX]);
+extern int substitute_binding(Tracee* tracee, Side side, char path[PATH_MAX]);
 
 #endif /* BINDING_H */

src/path/path.c
@@ -267,7 +267,7 @@ int translate_path(Tracee *tracee, char result[PATH_MAX],
  * including the end-of-string terminator.  On error it returns
  * -errno.
  */
-int detranslate_path(const Tracee *tracee, char path[PATH_MAX], const char t_referrer[PATH_MAX])
+int detranslate_path(Tracee *tracee, char path[PATH_MAX], const char t_referrer[PATH_MAX])
 {
 	size_t prefix_length;
 	size_t new_length;

src/path/path.h
@@ -66,7 +66,7 @@ typedef enum Comparison {
 } Comparison;
 
 extern int translate_path(Tracee *tracee, char result[PATH_MAX], int dir_fd, const char *fake_path, bool deref_final);
-extern int detranslate_path(const Tracee *tracee, char path[PATH_MAX], const char t_referrer[PATH_MAX]);
+extern int detranslate_path(Tracee *tracee, char path[PATH_MAX], const char t_referrer[PATH_MAX]);
 extern bool belongs_to_guestfs(const Tracee *tracee, const char *path);
 
 extern int join_paths(int number_paths, char result[PATH_MAX], ...);

src/tracee/event.c
@@ -225,7 +225,6 @@ static void print_talloc_hierarchy(int signum, siginfo_t *siginfo, void *ucontex
 			else if (strcmp(name, "Tracee") == 0) {
 				fprintf(stderr, "\t(pid = %d)", ((Tracee *)ptr)->pid);
 			}
-#if 0
 			else if (strcmp(name, "Bindings") == 0) {
 				 Tracee *tracee;
 
@@ -242,7 +241,6 @@ static void print_talloc_hierarchy(int signum, siginfo_t *siginfo, void *ucontex
 				Binding *binding = (Binding *)ptr;
 				fprintf(stderr, "\t(%s:%s)", binding->host.path, binding->guest.path);
 			}
-#endif
 		}
 
 		fprintf(stderr, "\n");
@@ -272,11 +270,6 @@ int event_loop()
 	int signum;
 	int signal;
 	pid_t pid;
-
-	/* Free everything when exiting.  */
-	status = atexit(free_bindings);
-	if (status != 0)
-		notice(WARNING, INTERNAL, "atexit() failed");
 
 	/* Kill all tracees when exiting.  */
 	status = atexit(kill_all_tracees);

src/tracee/tracee.c
@@ -85,10 +85,6 @@ Tracee *get_tracee(pid_t pid, bool create)
  */
 void inherit(Tracee *child, Tracee *parent)
 {
-	assert(child->exe  == NULL);
-	// assert(child->root == NULL);
-	// assert(child->cwd  == NULL);
-
 	/* The first tracee is started by PRoot and does nothing but a
 	 * call to execve(2), thus child->exe will be automatically
 	 * updated later.  */
@@ -100,8 +96,12 @@ void inherit(Tracee *child, Tracee *parent)
 		return;
 	}
 
+	assert(child->exe  == NULL);
+	assert(child->root == NULL);
+	// assert(child->cwd  == NULL);
+
 	assert(parent->exe  != NULL);
-	// assert(parent->root != NULL);
+	assert(parent->root != NULL);
 	// assert(parent->cwd  != NULL);
 
 	/* The path to the executable is unshared only once the child
@@ -138,6 +138,9 @@ void inherit(Tracee *child, Tracee *parent)
 		talloc_set_name_const(child->cwd, "$cwd");
 	}
 #else
+	child->bindings_user  = NULL; assert(parent->bindings_user == NULL);
+	child->bindings_guest = talloc_reference(child, parent->bindings_guest);
+	child->bindings_host  = talloc_reference(child, parent->bindings_host);
 	child->root = talloc_reference(child, parent->root);
 	child->qemu = talloc_reference(child, parent->qemu);
 	child->glue = talloc_reference(child, parent->glue);

src/tracee/tracee.h
@@ -40,6 +40,8 @@ typedef enum {
 	NB_REG_VERSION
 } RegVersion;
 
+struct bindings;
+
 /* Information related to a tracee process. */
 typedef struct tracee {
 	/**********************************************************************
@@ -80,8 +82,22 @@ typedef struct tracee {
 	mode_t glue_type;
 
 
+	/***********************************************************************
+	 * Shared resources until the tracee changes its filesystem namespace. *
+	 ***********************************************************************/
+
+	/* List of bindings as specified by the user.  */
+	struct bindings *bindings_user;
+
+	/* List of bindings canonicalized and sorted in the "guest" order.  */
+	struct bindings *bindings_guest;
+
+	/* List of bindings canonicalized and sorted in the "host" order.  */
+	struct bindings *bindings_host;
+
+
 	/**********************************************************************
-	 * Shared resources until the tracees makes a call to execve().       *
+	 * Shared resources until the tracee makes a call to execve().       *
 	 **********************************************************************/
 
 	/* Path to the executable, à la /proc/self/exe.  */

commit 9b3b38cf7107f82a70f712a386f04933d51bf721

Fix use-after-free when user tries to bind something to "/".
This regression was likely introduced by commit b0c54280.

Change-Id: I2783c843f1f838b02c2265279a69001cac54a2fb

src/path/binding.c
@@ -626,7 +626,7 @@ static int initialize_binding(Tracee *tracee, Binding *binding)
 
 	if (compare_paths(binding->guest.path, "/") == PATHS_ARE_EQUAL) {
 		if (LIST_FIRST(HEAD(tracee, HOST)) != NULL) {
-			notice(ERROR, USER, "explicit binding to \"/\" isn't allowed, "
+			notice(WARNING, USER, "explicit binding to \"/\" isn't allowed, "
 					    "use the -r option instead");
 			goto error;
 		}
@@ -691,7 +691,6 @@ static int initialize_binding(Tracee *tracee, Binding *binding)
 	return 0;
 
 error:
-	TALLOC_FREE(binding);
 	return -1;
 }
 
@@ -729,12 +728,16 @@ int initialize_bindings(Tracee *tracee)
 	 * @tracee->bindings_user.  */
 	while (binding != NULL) {
 		Binding *next;
+		int status;
 
 		next = LIST_NEXT(binding, user_link);
 
-		initialize_binding(tracee, binding);
+		status = initialize_binding(tracee, binding);
+		if (status < 0)
+			TALLOC_FREE(binding);
+		else
+			LIST_REMOVE_(binding, user_link);
 
-		LIST_REMOVE_(binding, user_link);
 		binding = next;
 	}
 

tests/test-aaaaaaaa.sh
@@ -30,3 +30,5 @@ ${PROOT} -r ${DONT_EXIST} -r ${ROOTFS} true
 
 ${PROOT} -b /bin/true:${DONT_EXIST} ${DONT_EXIST}
 
+${PROOT} -r / -b /etc:/ true
+${PROOT} -b /etc:/ true