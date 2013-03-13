proot-me/proot

commit 4117e5de6334e6e8210a5e1834e666ae20e940d5

Restore *all* registers but the result at the end of a syscall.
There are two exceptions: execve(2) and sigreturn(2).  Also, this
should improve the transparency and portability of PRoot: there's no
need to care about callee-saved registers.

src/tracee/reg.c
@@ -172,7 +172,7 @@ void poke_reg(Tracee *tracee, Reg reg, word_t value)
 		return;
 
 	REG(tracee, CURRENT, reg) = value;
-	tracee->_regs_have_changed = true;
+	tracee->_regs_were_changed = true;
 }
 
 /**
@@ -182,6 +182,7 @@ void poke_reg(Tracee *tracee, Reg reg, word_t value)
  */
 int fetch_regs(Tracee *tracee)
 {
+	const bool is_exit_stage = (tracee->status != 0);
 	int status;
 
 #if defined(ARCH_ARM64)
@@ -196,10 +197,14 @@ int fetch_regs(Tracee *tracee)
 #endif
 	if (status < 0)
 		return status;
-	tracee->_regs_have_changed = false;
 
-	if (tracee->status == 0)
-		memcpy(&tracee->_regs[ORIGINAL], &tracee->_regs[CURRENT], sizeof(struct user_regs_struct));
+	tracee->keep_current_regs = false;
+
+	if (!is_exit_stage) {
+		memcpy(&tracee->_regs[ORIGINAL], &tracee->_regs[CURRENT],
+			sizeof(tracee->_regs[CURRENT]));
+		tracee->_regs_were_changed = false;
+	}
 
 	return 0;
 }
@@ -211,9 +216,22 @@ int fetch_regs(Tracee *tracee)
  */
 int push_regs(Tracee *tracee)
 {
+	const bool is_exit_stage = (tracee->status == 0);
 	int status;
 
-	if (tracee->_regs_have_changed) {
+	if (tracee->_regs_were_changed) {
+		/* At very the end of a syscall, with regard to the
+		 * entry, only the result register can be modified by
+		 * a syscall (except for very special cases).  */
+		if (is_exit_stage && !tracee->keep_current_regs) {
+			word_t result = peek_reg(tracee, CURRENT, SYSARG_RESULT);
+
+			memcpy(&tracee->_regs[CURRENT], &tracee->_regs[ORIGINAL],
+				sizeof(tracee->_regs[ORIGINAL]));
+
+			poke_reg(tracee, SYSARG_RESULT, result);
+		}
+
 #if defined(ARCH_ARM64)
 		struct iovec regs;
 
@@ -228,8 +246,9 @@ int push_regs(Tracee *tracee)
 			return status;
 	}
 
-	if (tracee->status != 0)
-		memcpy(&tracee->_regs[MODIFIED], &tracee->_regs[CURRENT], sizeof(struct user_regs_struct));
+	if (!is_exit_stage)
+		memcpy(&tracee->_regs[MODIFIED], &tracee->_regs[CURRENT],
+			sizeof(tracee->_regs[CURRENT]));
 
 	return 0;
 }

src/syscall/exit.c
@@ -262,7 +262,7 @@ case PR_execve:
 	if ((int) syscall_result >= 0) {
 case PR_rt_sigreturn:
 case PR_sigreturn:
-		restore_original_sp = false;
+		tracee->keep_current_regs = true;
 	}
 	goto end;
 

src/syscall/syscall.c
@@ -264,7 +264,6 @@ end:
  */
 static void translate_syscall_exit(Tracee *tracee)
 {
-	bool restore_original_sp = true;
 	word_t syscall_number;
 	word_t syscall_result;
 	int status;
@@ -317,10 +316,6 @@ end:
 	status = notify_extensions(tracee, SYSCALL_EXIT_END, 0, 0);
 	if (status < 0)
 		poke_reg(tracee, SYSARG_RESULT, (word_t) status);
-
-	/* "restore_original_sp" was updated in syscall/exit.c.  */
-	if (restore_original_sp)
-		poke_reg(tracee, STACK_POINTER, peek_reg(tracee, ORIGINAL, STACK_POINTER));
 
 	VERBOSE(tracee, 3, "pid %d:        -> 0x%lx [0x%lx]", tracee->pid,
 		peek_reg(tracee, CURRENT, SYSARG_RESULT),

src/tracee/tracee.h
@@ -81,7 +81,8 @@ typedef struct tracee {
 
 	/* Value of the tracee's general purpose registers.  */
 	struct user_regs_struct _regs[NB_REG_VERSION];
-	bool _regs_have_changed;
+	bool _regs_were_changed;
+	bool keep_current_regs;
 
 	/* State for the special handling of SIGSTOP.  */
 	enum {

commit 8fbac6e35efc99a93833277959b9429c3c6d8bdb

Restore only the stack and sysarg registers at the end of a syscall.
Don't assume the kernel will not update the other registers.  Also,
there was a bug introduced by commit 4117e5de on x32, where
sizeof(word_t) == 4 but registers are 64-bit:

    poke_reg(peek_reg(result))

discards the sign extension.

src/tracee/reg.c
@@ -220,16 +220,18 @@ int push_regs(Tracee *tracee)
 	int status;
 
 	if (tracee->_regs_were_changed) {
-		/* At very the end of a syscall, with regard to the
+		/* At the very end of a syscall, with regard to the
 		 * entry, only the result register can be modified by
-		 * a syscall (except for very special cases).  */
+		 * PRoot.  */
 		if (is_exit_stage && !tracee->keep_current_regs) {
-			word_t result = peek_reg(tracee, CURRENT, SYSARG_RESULT);
-
-			memcpy(&tracee->_regs[CURRENT], &tracee->_regs[ORIGINAL],
-				sizeof(tracee->_regs[ORIGINAL]));
-
-			poke_reg(tracee, SYSARG_RESULT, result);
+			REG(tracee, CURRENT, SYSARG_NUM) = REG(tracee, ORIGINAL, SYSARG_NUM);
+			REG(tracee, CURRENT, SYSARG_1)   = REG(tracee, ORIGINAL, SYSARG_1);
+			REG(tracee, CURRENT, SYSARG_2)   = REG(tracee, ORIGINAL, SYSARG_2);
+			REG(tracee, CURRENT, SYSARG_3)   = REG(tracee, ORIGINAL, SYSARG_3);
+			REG(tracee, CURRENT, SYSARG_4)   = REG(tracee, ORIGINAL, SYSARG_4);
+			REG(tracee, CURRENT, SYSARG_5)   = REG(tracee, ORIGINAL, SYSARG_5);
+			REG(tracee, CURRENT, SYSARG_6)   = REG(tracee, ORIGINAL, SYSARG_6);
+			REG(tracee, CURRENT, STACK_POINTER) = REG(tracee, ORIGINAL, STACK_POINTER);
 		}
 
 #if defined(ARCH_ARM64)

src/tracee/reg.h
@@ -36,7 +36,6 @@ typedef enum {
 	SYSARG_6,
 	SYSARG_RESULT,
 	STACK_POINTER,
-	CS,
 } Reg;
 
 extern int fetch_regs(Tracee *tracee);