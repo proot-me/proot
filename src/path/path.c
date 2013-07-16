4322d0ab6c3a5fc25df3797b2e83b63cfcb8a1be
Cédric VINCENT <cedric.vincent@st.com>
2013-07-16 09:38:52 +0200

    Fix join_paths(): out-of-bounds read from a buffer.
      src/path/path.c:join_paths()
    
      177                else if (need_separator == 1) {
      178                        strcat(result + old_length, "/");
    
      >>> 19. incr: Incrementing "old_length". The value of "old_length"
      >>> is now between 2 and 4096 (inclusive).
    
      179                        old_length++;
      180                }
    
      >>> CID 1047031 (#1 of 1): Out-of-bounds read (OVERRUN)
    
      >>> 20. overrun-local: Overrunning array of 4096 bytes at
      >>> byte offset 4096 by dereferencing pointer "result + old_length".
    
      181                strcat(result + old_length, path);

--- src/path/path.c
+++ src/path/path.c
@@ -140,6 +140,7 @@ int join_paths(int number_paths, char result[PATH_MAX], ...)
 {
 	va_list paths;
 	size_t length;
+	int status;
 	int i;
 
 	result[0] = '\0';
@@ -156,6 +157,12 @@ int join_paths(int number_paths, char result[PATH_MAX], ...)
 		if (path == NULL)
 			continue;
 
+		/* Sanity check.  */
+		if (length >= PATH_MAX ) {
+			status = -ENAMETOOLONG;
+			break;
+		}
+
 		/* Check if a path separator is needed. */
 		if (length > 0 && result[length - 1] != '/' && path[0] != '/')
 			need_separator = 1;
@@ -166,23 +173,33 @@ int join_paths(int number_paths, char result[PATH_MAX], ...)
 
 		old_length = length;
 		length += strlen(path) + need_separator;
-		if (length >= PATH_MAX) {
-			va_end(paths);
-			return -ENAMETOOLONG;
-		}
 
-		if (need_separator == -1) {
+		switch (need_separator) {
+		case -1:
 			path++;
-		}
-		else if (need_separator == 1) {
+			break;
+		case 1:
 			strcat(result + old_length, "/");
 			old_length++;
+			break;
+		case 0:
+			break;
+		default:
+			assert(0);
 		}
+
+		/* Sanity check.  */
+		if (old_length >= PATH_MAX) {
+			status = -ENAMETOOLONG;
+			break;
+		}
+
 		strcat(result + old_length, path);
+		status = 0;
 	}
 	va_end(paths);
 
-	return 0;
+	return status;
 }
 
 /**
