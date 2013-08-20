3e34cdbd5f2640c753897decc85f25f1774a3fd0
Cédric VINCENT <cedric.vincent@st.com>
2013-08-20 13:33:41 +0200

    Fix the sysexit handling of rename[at] when used to move [a prefix of] cwd.
    Before:
    
        $ proot ./ltp-full-20130503/testcases/kernel/syscalls/inotify/inotify02
        inotify02    8  TBROK  :  unlink(test_file2) failed: errno=ENOENT(2): No such file or directory
        inotify02    9  TBROK  :  Remaining cases broken
        inotify02    0  TWARN  :  tst_rmdir: rmobj(/tmp/inoHPzj80) failed: lstat(/tmp/inoHPzj80) failed; errno=2: No such file or directory
    
    Now:
    
        $ proot ./ltp-full-20130503/testcases/kernel/syscalls/inotify/inotify02
        inotify02    1  TPASS  :  get event: wd=1 mask=40000004 cookie=0 len=0 name=""
        inotify02    2  TPASS  :  get event: wd=1 mask=100 cookie=0 len=16 name="test_file1"
        inotify02    3  TPASS  :  get event: wd=1 mask=20 cookie=0 len=16 name="test_file1"
        inotify02    4  TPASS  :  get event: wd=1 mask=8 cookie=0 len=16 name="test_file1"
        inotify02    5  TPASS  :  get event: wd=1 mask=40 cookie=11309409 len=16 name="test_file1"
        inotify02    6  TPASS  :  get event: wd=1 mask=80 cookie=11309409 len=16 name="test_file2"
        inotify02    7  TPASS  :  get event: wd=1 mask=800 cookie=0 len=0 name=""
        inotify02    8  TPASS  :  get event: wd=1 mask=200 cookie=0 len=16 name="test_file2"
        inotify02    9  TPASS  :  get event: wd=1 mask=800 cookie=0 len=0 name=""

--- src/syscall/exit.c
+++ src/syscall/exit.c
@@ -213,6 +213,83 @@ void translate_syscall_exit(Tracee *tracee)
 		status = 0;
 		break;
 
+	case PR_rename:
+	case PR_renameat: {
+		char old_path[PATH_MAX];
+		char new_path[PATH_MAX];
+		ssize_t old_length;
+		ssize_t new_length;
+		Comparison comparison;
+		Reg old_reg;
+		Reg new_reg;
+		char *tmp;
+
+		/* Error reported by the kernel.  */
+		if ((int) syscall_result < 0)
+			goto end;
+
+		if (syscall_number == PR_rename) {
+			old_reg = SYSARG_1;
+			new_reg = SYSARG_2;
+		}
+		else {
+			old_reg = SYSARG_2;
+			new_reg = SYSARG_3;
+		}
+
+		/* Get the old path, then convert it to the same
+		 * "point-of-view" as tracee->fs->cwd (guest).  */
+		status = read_path(tracee, old_path, peek_reg(tracee, MODIFIED, old_reg));
+		if (status < 0)
+			break;
+
+		status = detranslate_path(tracee, old_path, NULL);
+		if (status < 0)
+			break;
+		old_length = (status > 0 ? status - 1 : (ssize_t) strlen(old_path));
+
+		/* Nothing special to do if the moved path is not the
+		 * current working directory.  */
+		comparison = compare_paths(old_path, tracee->fs->cwd);
+		if (comparison != PATH1_IS_PREFIX && comparison != PATHS_ARE_EQUAL) {
+			status = 0;
+			break;
+		}
+
+		/* Get the new path, then convert it to the same
+		 * "point-of-view" as tracee->fs->cwd (guest).  */
+		status = read_path(tracee, new_path, peek_reg(tracee, MODIFIED, new_reg));
+		if (status < 0)
+			break;
+
+		status = detranslate_path(tracee, new_path, NULL);
+		if (status < 0)
+			break;
+		new_length = (status > 0 ? status - 1 : (ssize_t) strlen(new_path));
+
+		/* Sanity check.  */
+		if (strlen(tracee->fs->cwd) >= PATH_MAX) {
+			status = 0;
+			break;
+		}
+		strcpy(old_path, tracee->fs->cwd);
+
+		/* Update the virtual current working directory.  */
+		substitute_path_prefix(old_path, old_length, new_path, new_length);
+
+		tmp = talloc_strdup(tracee->fs, old_path);
+		if (tmp == NULL) {
+			status = -ENOMEM;
+			break;
+		}
+
+		TALLOC_FREE(tracee->fs->cwd);
+		tracee->fs->cwd = tmp;
+
+		status = 0;
+		break;
+	}
+
 	case PR_readlink:
 	case PR_readlinkat: {
 		char referee[PATH_MAX];
