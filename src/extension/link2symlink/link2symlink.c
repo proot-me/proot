bccc0c888aa8568a38096dc3064862bf566d7c1f
Cédric VINCENT <cedric.vincent@st.com>
2013-10-16 21:19:54 +0200

    link2symlink: use an intermediate file that holds the original content.
    As said by Corbin Champion, hardlinks and symlinks are not
    semantically equivalent:
    
      * for the hardlink "A <-> B", A and B points to the same content.
        If one of them is removed, the other one is still valid.
    
      * for the symlink "A -> B ", A points to B.  If the latter is
        removed, the former will be invalid.
    
    To keep the original semantic, an intermediate file could be used: the
    hardlink "A <-> B" can be translated into two symlinks ""A -> C" and
    "B -> C", where C is the original content.
    
    Before:
    
        $ rm -f foo bar
        $ echo TEST > foo
        $ ./src/proot ln foo bar
        $ ls -il foo bar
        6127676 lrwxrwxrwx 1 cedric users 35 Oct 16 21:21 bar -> /home/user/foo
        6127675 -rw-r--r-- 1 cedric users 5 Oct 16 21:21 /home/user/foo
    
    Now:
    
        $ rm -f foo bar
        $ echo TEST > foo
        $ ./src/proot ln foo bar
        $ ls -il foo bar
        6127676 lrwxrwxrwx 1 cedric users 35 Oct 16 21:21 bar -> /home/user/foo.l2s
        6127669 lrwxrwxrwx 1 cedric users 35 Oct 16 21:21 foo -> /home/user/foo.l2s

--- src/extension/link2symlink/link2symlink.c
+++ src/extension/link2symlink/link2symlink.c
@@ -1,8 +1,84 @@
+#include <stdio.h>     /* rename(2), */
+#include <unistd.h>    /* symlink(2), readlink(2), lstat(2), */
+#include <string.h>    /* str*, */
+#include <sys/types.h> /* lstat(2), */
+#include <sys/stat.h>  /* lstat(2), */
+#include <errno.h>     /* E*, */
+#include <limits.h>    /* PATH_MAX, */
+
 #include "extension/extension.h"
 #include "tracee/tracee.h"
+#include "tracee/mem.h"
+#include "syscall/syscall.h"
 #include "syscall/sysnum.h"
 #include "arch.h"
 #include "attribute.h"
+
+#define SUFFIX ".l2s"
+
+/**
+ * Move the path pointed to by @tracee's @sysarg to a new location,
+ * symlink the original path to this new one, make @tracee's @sysarg
+ * point to the new location.  This function returns -errno if an
+ * error occured, otherwise 0.
+ */
+static int move_and_symlink_path(Tracee *tracee, Reg sysarg)
+{
+	char original[PATH_MAX];
+	char moved[PATH_MAX];
+	struct stat statl;
+	size_t length;
+	ssize_t size;
+	int status;
+
+	/* Note: this path was already canonicalized.  */
+	size = read_string(tracee, original, peek_reg(tracee, CURRENT, sysarg), PATH_MAX);
+	if (size < 0)
+		return size;
+	if (size >= PATH_MAX)
+		return -ENAMETOOLONG;
+
+	/* Sanity check: directories can't be linked.  */
+	status = lstat(original, &statl);
+	if (status < 0)
+		return status;
+	if (S_ISDIR(statl.st_mode))
+		return -EPERM;
+
+	length = strlen(original);
+
+	/* Check if it is a converted link already.  */
+	if (strcmp(original + length - strlen(SUFFIX), SUFFIX) == 0) {
+		size = readlink(original, moved, PATH_MAX);
+		if (size < 0)
+			return size;
+		if (size >= PATH_MAX)
+			return -ENAMETOOLONG;
+	}
+	else {
+		/* Compute the new path.  */
+		if (strlen(SUFFIX) + strlen(original) >= PATH_MAX)
+			return -ENAMETOOLONG;
+		strcpy(moved, original);
+		strcat(moved, SUFFIX);
+
+		/* Move the original content to the new path.  */
+		status = rename(original, moved);
+		if (status < 0)
+			return status;
+
+		/* Symlink the original path back to the new one.  */
+		status = symlink(moved, original);
+		if (status < 0)
+			return status;
+	}
+
+	status = set_sysarg_path(tracee, moved, sysarg);
+	if (status < 0)
+		return status;
+
+	return 0;
+}
 
 /**
  * Handler for this @extension.  It is triggered each time an @event
@@ -11,6 +87,8 @@
 int link2symlink_callback(Extension *extension, ExtensionEvent event,
 				  intptr_t data1 UNUSED, intptr_t data2 UNUSED)
 {
+	int status;
+
 	switch (event) {
 	case INITIALIZATION: {
 		/* List of syscalls handled by this extensions.  */
@@ -25,7 +103,7 @@ int link2symlink_callback(Extension *extension, ExtensionEvent event,
 
 	case SYSCALL_ENTER_END: {
 		Tracee *tracee = TRACEE(extension);
-		
+
 		switch (get_sysnum(tracee, ORIGINAL)) {
 		case PR_link:
 			/* Convert:
@@ -36,10 +114,15 @@ int link2symlink_callback(Extension *extension, ExtensionEvent event,
 			 *
 			 *     int symlink(const char *oldpath, const char *newpath);
 			 */
+
+			status = move_and_symlink_path(tracee, SYSARG_1);
+			if (status < 0)
+				return status;
+
 			set_sysnum(tracee, PR_symlink);
 			break;
 
-		case PR_linkat: {
+		case PR_linkat:
 			/* Convert:
 			 *
 			 *     int linkat(int olddirfd, const char *oldpath,
@@ -56,14 +139,15 @@ int link2symlink_callback(Extension *extension, ExtensionEvent event,
 			 *   newdirfd + newpath -> newpath
 			 */
 
-			word_t old = peek_reg(tracee, CURRENT, SYSARG_2);
-			word_t new = peek_reg(tracee, CURRENT, SYSARG_4);
+			status = move_and_symlink_path(tracee, SYSARG_2);
+			if (status < 0)
+				return status;
 
-			poke_reg(tracee, SYSARG_1, old);
-			poke_reg(tracee, SYSARG_2, new);
+			poke_reg(tracee, SYSARG_1, peek_reg(tracee, CURRENT, SYSARG_2));
+			poke_reg(tracee, SYSARG_2, peek_reg(tracee, CURRENT, SYSARG_4));
+
 			set_sysnum(tracee, PR_symlink);
 			break;
-		}
 
 		default:
 			break;
