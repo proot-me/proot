ddd459d8e6768c07eb73a258949312e1bc2d5e17
Cédric VINCENT <cedric.vincent@st.com>
2014-08-05 10:32:37 +0200

    Fix a portability issue in test-dddddddd.sh on Debian-based systems.
    The "-e" option of realpath does not exist on Debian-based systems but
    it doesn't return a failure...
    
    Reported-by: Rémi Duraffort <remi.duraffort@st.com>

--- tests/test-dddddddd.sh
+++ tests/test-dddddddd.sh
@@ -2,8 +2,10 @@ if [ -z `which mcookie` ] || [ -z `which rm` ] || [ -z `which ln` ] || [ -z `whi
     exit 125;
 fi
 
-! realpath -e /proc/self/exe
-if [ $? != 1 ]; then
+CHECK1=$(realpath -e /proc/self/exe)
+CHECK2=$(realpath /proc/self/exe)
+
+if [ "${CHECK1}" != "${CHECK2}" ]; then
     exit 125;
 fi
 
