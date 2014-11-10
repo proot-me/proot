3bf9316a161c6f60d77d2cb99f4b87da82823268
Cédric VINCENT <cedric.vincent@st.com>
2014-11-10 09:05:41 +0100

    Improve test-5996858d portability: on x86, AT_HWCAP can be "" instead of "0".

--- tests/test-5996858d.sh
+++ tests/test-5996858d.sh
@@ -9,7 +9,7 @@ ${PROOT} -k ${UTSNAME} uname -n | grep ^nodename$
 ${PROOT} -k ${UTSNAME} uname -v | grep ^version$
 ${PROOT} -k ${UTSNAME} uname -m | grep ^machine$
 ${PROOT} -k ${UTSNAME} domainname | grep ^domainname$
-${PROOT} -k ${UTSNAME} env LD_SHOW_AUXV=1 true | grep '^AT_HWCAP:[[:space:]]*0$'
+${PROOT} -k ${UTSNAME} env LD_SHOW_AUXV=1 true | grep -E '^AT_HWCAP:[[:space:]]*0?$'
 
 ${PROOT} -0 -k ${UTSNAME} sh -c 'domainname domainname2; domainname' | grep ^domainname2$
 ${PROOT} -0 -k ${UTSNAME} sh -c 'hostname hostname2; hostname' | grep ^hostname2$
